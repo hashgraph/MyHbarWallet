c6cc9334b600a21126c61bbeb4a462c27bcb793f
Ryan Leckey <ryan@launchbadge.com>
2019-07-26 12:11:01 -0700

    Initial commit (from generator)

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld name="World" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "home",
+  components: {
+    HelloWorld
+  }
+});
+</script>
