f272dfcdeac4b9c5951f72baa9384d598ad24592
Ryan Leckey <ryan@launchbadge.com>
2019-07-26 13:23:45 -0700

    Turn on stage 0 (nesting) and setup a gloal CSS file

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    plugins: [require("postcss-preset-env")]
+    plugins: [
+        require("postcss-preset-env")({
+            stage: 0,
+            importFrom: ["./src/main.css"]
+        })
+    ]
 };
