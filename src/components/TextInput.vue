e98424a3714936a88490c893af8e567fd0b45fc8
Ryan Leckey <ryan@launchbadge.com>
2019-07-27 16:53:52 -0700

    Add eye toggle to TextInput and add some tests

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,23 +1,61 @@
 <template>
     <div class="input-container">
-        <input :placeholder="placeholder" :type="keyboardType" />
+        <input
+            ref="input"
+            :placeholder="placeholder"
+            :type="keyboardType"
+            :value="value"
+            @input="handleInput"
+        />
+        <FontAwesomeIcon
+            @click="handleClickEye"
+            v-if="obscure"
+            class="eye"
+            :class="{ 'is-open': isEyeOpen }"
+            :icon="faEye"
+        />
     </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faEye, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 export default Vue.extend({
     props: {
         placeholder: String,
+        value: String,
 
         // Whether to hide the text being edited (e.g., for passwords).
         obscure: Boolean
     },
+    components: {
+        FontAwesomeIcon
+    },
+    data() {
+        return {
+            faEye,
+
+            // If the eye is open to show the obscured text anyway
+            isEyeOpen: false
+        };
+    },
     computed: {
         keyboardType() {
-            if (this.obscure) return "password";
+            if (this.obscure && !this.isEyeOpen) return "password";
             return "text";
+        }
+    },
+    methods: {
+        handleInput(event: Event) {
+            this.$emit("input", (event.target as HTMLInputElement).value);
+        },
+        handleClickEye() {
+            this.isEyeOpen = !this.isEyeOpen;
+
+            // Re-focus the input (loses focus from the tap on the eye)
+            (this.$refs.input as HTMLInputElement).focus();
         }
     }
 });
@@ -37,9 +75,29 @@ input {
     outline: none;
     border: 2px solid var(--color-peral);
     color: var(--color-washed-black);
+    width: 100%;
 
     &:focus {
         border-color: var(--color-melbourne-cup);
+    }
+
+    &:not(:only-child) {
+        padding-right: 50px;
+    }
+}
+
+.eye {
+    opacity: 0.3;
+    cursor: pointer;
+    position: absolute;
+    right: 20px;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+
+    &.is-open {
+        opacity: 1;
+        color: var(--color-melbourne-cup);
     }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,12 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld name="World" />
-
-        <div class="section">
-            <Button label="Choose a Hardware" />
-            <Button label="Next" :rightIcon="faArrowRight" />
-            <Button
-                label="Access Wallet"
-                @click="handleAccessWallet"
-                :isBusy="isAccessWalletBusy"
-            />
-        </div>
-
-        <div class="section">
-            <TextInput obscure placeholder="Enter password" />
-        </div>
-    </div>
+    <div class="home"></div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
-import Button from "@/components/Button.vue";
-import TextInput from "@/components/TextInput.vue";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default Vue.extend({
-    name: "home",
-    components: {
-        HelloWorld,
-        Button,
-        TextInput
-    },
-    data() {
-        return {
-            isAccessWalletBusy: false
-        };
-    },
-    computed: {
-        faArrowRight() {
-            return faArrowRight;
-        }
-    },
-    methods: {
-        handleAccessWallet() {
-            this.isAccessWalletBusy = true;
-
-            setTimeout(() => {
-                this.isAccessWalletBusy = false;
-            }, 1500);
-        }
-    }
+    name: "home"
 });
 </script>
 
@@ -60,16 +15,5 @@ export default Vue.extend({
     display: flex;
     flex-direction: column;
     align-items: center;
-}
-
-.section {
-    margin-top: 25px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.section > * {
-    margin-top: 25px;
 }
 </style>
