cb98ed28934267fb14e54d9b96caac8f90908573
Ryan Leckey <ryan@launchbadge.com>
2019-07-27 21:20:52 -0700

    Extend #app and set the background color

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 
 <style lang="postcss" scoped>
 #app {
+    background-color: var(--color-peral);
+    min-height: inherit;
+
     /* https://fonts.google.com/specimen/Montserrat */
     font-family: "Montserrat", sans-serif;
 }
