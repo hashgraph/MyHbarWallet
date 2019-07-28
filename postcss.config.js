0d05c7e2a129e3e3e1e5177f8d73ad7ddeb85a8c
Ryan Leckey <ryan@launchbadge.com>
2019-07-27 21:01:15 -0700

    Setup Font Magician and import Montserrat from Google Fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = {
     plugins: [
         require("postcss-normalize")(),
+        require('postcss-font-magician')({
+            variants: {
+                'Montserrat': {
+                    '300': [],
+                    '400': [],
+                    '500': [],
+                    '600': [],
+                    '700': []
+                }
+            },
+            foundries: ['google'],
+        }),
         require("postcss-preset-env")({
             stage: 0,
             importFrom: ["./src/main.css"]
--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,10 @@
         <router-view />
     </div>
 </template>
+
+<style lang="postcss" scoped>
+#app {
+    /* https://fonts.google.com/specimen/Montserrat */
+    font-family: "Montserrat", sans-serif;
+}
+</style>
