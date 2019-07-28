faa151aeb03ea37b92d2324ab33095c0d72cfd31
Ryan Leckey <ryan@launchbadge.com>
2019-07-27 22:11:34 -0700

    Enforce additional Vue-specific rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,25 @@ module.exports = {
     rules: {
         indent: ["error", 4],
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+        // Vue
+        // https://eslint.vuejs.org/rules/#uncategorized
+        "vue/array-bracket-spacing": "error",
+        "vue/arrow-spacing": "error",
+        "vue/block-spacing": "error",
+        "vue/brace-style": "error",
+        "vue/camelcase": "error",
+        "vue/comma-dangle": "error",
+        "vue/component-name-in-template-casing": "error",
+        "vue/eqeqeq": "error",
+        "vue/key-spacing": "error",
+        "vue/match-component-file-name": "error",
+        "vue/no-boolean-default": "error",
+        "vue/object-curly-spacing": ["error", "always"],
+        "vue/script-indent": ["error", 4],
+        "vue/space-infix-ops": "error",
+        "vue/space-unary-ops": "error",
+        "vue/v-on-function-call": "error"
     }
 };
