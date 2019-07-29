98f4eccf61658ffc4c8c552dd94fda0b1c807358
Kristy <kristy@launchbadge.com>
2019-07-29 11:52:55 -0700

    Clean up scroll logic and make general improvements for header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <Header></Header>
+        <Header />
         <router-view />
         <Footer />
     </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
-    <div v-scroll="onScroll" class="header-container">
-        <div class="header">
+    <div
+        v-scroll="onScroll"
+        :class="{ scrolled: isScrolled }"
+        class="header-container"
+    >
+        <header>
             <router-link to="/" class="link">
                 <div class="logo">MHW</div>
             </router-link>
@@ -10,42 +14,34 @@
                 <router-link to="" class="link">About</router-link>
                 <router-link to="" class="link">FAQs</router-link>
             </div>
-        </div>
+        </header>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-Vue.directive("scroll", {
-    inserted: function(el, binding) {
-        let f = function() {
-            if (binding.value(el)) {
-                window.removeEventListener("scroll", f);
-            }
-        };
-        window.addEventListener("scroll", f);
-    }
-});
-
 export default Vue.extend({
     name: "Header",
     data() {
         return {
-            listener: null
+            isScrolled: false
         };
     },
-    methods: {
-        onScroll: function(el: HTMLElement): boolean {
-            if (!el.classList.contains("scrolled") && window.scrollY > 100) {
-                el.classList.toggle("scrolled");
-            } else if (
-                el.classList.contains("scrolled") &&
-                window.scrollY < 80
-            ) {
-                el.classList.toggle("scrolled");
+    directives: {
+        scroll: {
+            inserted(el, binding) {
+                window.addEventListener("scroll", binding.value);
             }
-            return false;
+        }
+    },
+    methods: {
+        onScroll() {
+            if (window.scrollY > 100) {
+                this.isScrolled = true;
+            } else if (window.scrollY < 80) {
+                this.isScrolled = false;
+            }
         }
     }
 });
@@ -57,43 +53,43 @@ export default Vue.extend({
     position: sticky;
     top: 0;
     transition: all 0.3s ease;
+    z-index: 1;
 
-    /* Don't believe IntelliJ this is totally being used */
+    /* Don't believe IntelliJ, this is totally being used */
     &.scrolled {
         box-shadow: 0 2px 13px rgba(0, 0, 0, 0.1);
-
-        & .header {
-            padding-top: 3px;
-            padding-bottom: 3px;
-        }
     }
 }
 
-.header {
+.scrolled header {
+    padding-top: 3px;
+    padding-bottom: 3px;
+}
+
+header {
     display: flex;
     padding: 12px 25px;
     max-width: 1024px;
-    margin-left: auto;
-    margin-right: auto;
+    margin: 0 auto;
     transition: all 0.3s ease;
+}
+
+.link {
+    text-decoration: none;
+    color: var(--color-china-blue);
+
+    &:hover {
+        cursor: pointer;
+    }
+}
+
+.header-links {
+    display: flex;
+    font-size: 14px;
 
     & .link {
-        text-decoration: none;
-        color: var(--color-china-blue);
-
-        &:hover {
-            cursor: pointer;
-        }
-    }
-
-    & .header-links {
-        display: flex;
-        font-size: 14px;
-
-        & .link {
-            padding: 7px 14px;
-            margin-left: 26px;
-        }
+        padding: 7px 14px;
+        margin-inline-start: 26px;
     }
 }
 
