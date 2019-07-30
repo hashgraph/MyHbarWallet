29c1e982abc6b3d52d02ac796773111e8acd653c
Chris Campbell <chris@launchbadge.com>
2019-07-30 15:44:49 -0700

    click state change test for radiobutton

--- tests/unit/radiobutton.spec.ts
+++ tests/unit/radiobutton.spec.ts
@@ -45,4 +45,31 @@ describe("RadioButton.vue", () => {
         expect(fn).toBeCalledTimes(1);
         expect(wrapper.emitted("change")[0][0]).toEqual(_value);
     });
+
+    it("changes state in response to click", () => {
+        const _name = "RadioGroup";
+        const _label = "Label";
+        const _value = "Value";
+        const fn = jest.fn();
+
+        const wrapper = shallowMount(RadioButton, {
+            propsData: {
+                name: _name,
+                label: _label,
+                value: _value,
+                image: imageKey
+            },
+            listeners: {
+                click: fn
+            }
+        });
+
+        const radio = wrapper.find("input[type='radio']");
+
+        // You need both of the below to click and check the input
+        radio.setChecked(true);
+        radio.trigger("change");
+
+        expect(wrapper.find('radio-button').classes()).toContain("is-selected");
+    })
 });
