98ec7c17e908ba1b15442c4f439fb0d5f81a20ec
Ryan Leckey <ryan@launchbadge.com>
2019-07-30 16:47:18 -0700

    Implement remainder of Mnemonic control and localize to just that control

--- src/components/Mnemonic.vue
+++ src/components/Mnemonic.vue
@@ -1,209 +1,109 @@
 <template>
-    <div class="container">
-        <div class="password-title">
-            <div class="text">Your Password</div>
-            <div class="icon" />
-        </div>
-        <div class="header">
-            <Toggle lefttext="12" righttext="24" :colored="true" />
-            <div class="refresh-container">
-                <div class="icon" />
-                <div class="text">Random</div>
-            </div>
-        </div>
-        <div class="list-container">
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">1.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">10.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">11.</span><span class="text">sup</span>
-            </div>
-            <div class="list-item">
-                <span class="number">12.</span><span class="text">sup</span>
-            </div>
-        </div>
-        <div class="password-input">
-            <span class="text">Password</span>
-            <div class="optional">
-                <div class="text">Optional</div>
-                <Toggle />
-            </div>
-        </div>
-        <Button class="phrase-button" label="I Wrote Down My Mnemonic Phrase" />
-        <div class="footer">
-            <span class="red">DO NOT FORGET</span>
-            to save your password. You will need this
-        </div>
+    <div class="list-container">
+        <label
+            v-for="index in words"
+            :key="index"
+            class="list-item"
+            :class="{ 'is-focused': focused === index }"
+        >
+            <span class="number">{{ index }}.</span>
+
+            <input
+                class="word"
+                :value="value.length < index ? '' : value[index - 1]"
+                :readonly="!editable"
+                :data-index="index"
+                :tabindex="editable ? null : -1"
+                @input="handleInput"
+                @focus="handleFocus"
+            />
+        </label>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Toggle from "@/components/Toggle.vue";
-import Button from "@/components/Button.vue";
+import Vue, { PropOptions } from "vue";
 
 export default Vue.extend({
-    name: "Mnemonic",
-    components: {
-        Toggle,
-        Button
+    props: {
+        editable: { type: Boolean },
+        words: { type: Number, required: true },
+        value: { type: Array, required: true } as PropOptions<string[]>
+    },
+    data() {
+        return {
+            focused: null as (number | null)
+        };
+    },
+    methods: {
+        handleInput(event: Event) {
+            let target = event.target as HTMLInputElement;
+            let index = Number.parseInt(target.dataset.index!, 10);
+
+            let newValues = this.value.slice();
+            newValues[index - 1] = target.value;
+
+            this.$emit("input", newValues);
+        },
+
+        handleFocus(event: Event) {
+            if (!this.editable) {
+                // Non-editable controls should not set focus
+                return;
+            }
+
+            let target = event.target as HTMLInputElement;
+            this.focused = Number.parseInt(target.dataset.index!, 10);
+        }
     }
 });
 </script>
 
 <style scoped lang="postcss">
-.optional {
-    align-items: center;
-    display: flex;
-
-    & > .text {
-        color: var(--color-melbourne-cup);
-        margin-inline-end: 10px;
-    }
-}
-
-.header {
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-}
-
-.refresh-container {
-    align-items: center;
-    display: flex;
-
-    & > .icon {
-        background-color: var(--color-melbourne-cup);
-        height: 20px;
-        margin-inline-end: 5px;
-        width: 20px;
-    }
-
-    & > .text {
-        color: var(--color-melbourne-cup);
-        font-size: 15px;
-    }
-}
-
-.footer {
-    color: var(--color-ghostlands-coal);
-    font-size: 15px;
-
-    & > .red {
-        color: var(--color-lightish-red);
-        font-size: 16px;
-    }
-}
-
-.phrase-button {
-    align-items: center;
-    display: flex;
-    margin-block-end: 20px;
-    width: 100%;
-}
-
-.password-title {
-    display: flex;
-    margin-block-end: 40px;
-
-    & > .text {
-        align-items: center;
-        display: flex;
-        font-size: 22px;
-        justify-content: center;
-        margin-inline-end: 10px;
-        text-align: center;
-    }
-
-    & > .icon {
-        background-color: var(--color-basalt-grey);
-        height: 25px;
-        width: 25px;
-    }
-}
-
-.password-input {
-    border-bottom: 1px solid var(--color-zhen-zhu-bai-pearl);
-    border-top: 1px solid var(--color-zhen-zhu-bai-pearl);
-    display: flex;
-    justify-content: space-between;
-    margin-block-end: 20px;
-    padding: 20px 0;
-    width: 100%;
-
-    & > .text {
-        color: var(--color-ghostlands-coal);
-        font-size: 20px;
-        font-weight: bold;
-    }
-}
-
-.container {
-    align-items: center;
-    background-color: var(--color-white);
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 550px;
-    padding: 50px;
-}
-
 .list-container {
     align-items: center;
+    display: grid;
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+    grid-template-columns: 1fr 1fr 1fr;
+
+    @media (max-width: 414px) {
+        grid-template-columns: 1fr 1fr;
+    }
+}
+
+.list-item {
+    align-items: center;
+    border-bottom: 1px solid var(--color-jupiter);
+    color: var(--color-basalt-grey);
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    margin-block-end: 40px;
-    max-width: 450px;
+    font-size: 14px;
+    padding: 10px 0;
 
-    & > .list-item {
-        border-bottom: 1px solid var(--color-jupiter);
-        color: var(--color-basalt-grey);
-        flex-grow: 0;
-        flex-shrink: 1;
-        font-size: 18px;
-        margin-block-end: 15px;
-        padding: 15px 0;
-        text-align: start;
-        width: 29%;
+    &.is-focused {
+        border-color: var(--color-melbourne-cup);
+    }
+}
 
-        & > .number {
-            margin-inline-end: 10px;
-        }
+.number {
+    flex-shrink: 0;
+    font-size: 14px;
+}
 
-        & > .text {
-            color: var(--color-ghostlands-coal);
-        }
+.list-item.is-focused .number {
+    color: var(--color-melbourne-cup);
+}
+
+.word {
+    border: none;
+    color: var(--color-ghostlands-coal);
+    margin-inline-start: 10px;
+    outline: none;
+    padding: 0;
+    width: 100%;
+
+    &:read-only {
+        cursor: default;
     }
 }
 </style>
