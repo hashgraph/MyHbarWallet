50c8ed0598be6d8686eb0e63a7cfa308a4541282
Chris Campbell <chris@launchbadge.com>
2019-07-30 16:04:13 -0700

    update, merge tests

--- tests/unit/radiobutton.spec.ts
+++ tests/unit/radiobutton.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import RadioButton from "@/components/RadioButton.vue";
 import imageKey from "@/assets/button-key.svg";
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 describe("RadioButton.vue", () => {
     it("renders, contains expected properties", () => {
@@ -14,16 +14,18 @@ describe("RadioButton.vue", () => {
                 name: _name,
                 label: _label,
                 value: _value,
-                image: imageKey
+                image: imageKey,
+                checked: _value
             }
         });
 
         expect(wrapper.isVisible()).toBe(true);
-        expect(wrapper.find('label').text()).toEqual(_label);
-        expect(wrapper.find('input').attributes()['name']).toEqual(_name);
+        expect(wrapper.find("label").text()).toEqual(_label);
+        expect(wrapper.find("input").attributes()["name"]).toEqual(_name);
+        expect(wrapper.find("img").attributes()["src"]).toEqual(imageKey);
     });
 
-    it("triggers a click, emits its value", () => {
+    it("reacts when checked", () => {
         const _name = "RadioGroup";
         const _label = "Label";
         const _value = "Value";
@@ -34,42 +36,18 @@ describe("RadioButton.vue", () => {
                 name: _name,
                 label: _label,
                 value: _value,
-                image: imageKey
+                image: imageKey,
+                checked: _value
             },
             listeners: {
-                click: fn
+                change: fn
             }
         });
 
-        wrapper.find('input').trigger("click");
+        wrapper.find("input").setChecked(true);
         expect(fn).toBeCalledTimes(1);
         expect(wrapper.emitted("change")[0][0]).toEqual(_value);
+        expect(wrapper.find('label').classes()).toContain("is-selected");
+        expect(wrapper.contains(FontAwesomeIcon)).toBe(true);  // green check
     });
-
-    it("changes state in response to click", () => {
-        const _name = "RadioGroup";
-        const _label = "Label";
-        const _value = "Value";
-        const fn = jest.fn();
-
-        const wrapper = shallowMount(RadioButton, {
-            propsData: {
-                name: _name,
-                label: _label,
-                value: _value,
-                image: imageKey
-            },
-            listeners: {
-                click: fn
-            }
-        });
-
-        const radio = wrapper.find("input[type='radio']");
-
-        // You need both of the below to click and check the input
-        radio.setChecked(true);
-        radio.trigger("change");
-
-        expect(wrapper.find('radio-button').classes()).toContain("is-selected");
-    })
 });
--- tests/unit/radiobuttongroup.spec.ts
+++ tests/unit/radiobuttongroup.spec.ts
@@ -3,5 +3,15 @@ import Button from "../../src/components/RadioButtonGroup.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 describe("RadioButtonGroup.vue", () => {
+    it("renders with an empty list", () => {
 
+    });
+
+    it("renders with a list", () => {
+
+    });
+
+    it("triggers appropriate events", () => {
+
+    })
 });
