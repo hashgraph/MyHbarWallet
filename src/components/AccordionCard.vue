0e93d332b6839c132c34bb40b1982bbb91752fa2
Chris Campbell <chris@launchbadge.com>
2019-07-31 10:52:52 -0700

    WIP: Accordion

---
+++ src/components/AccordionCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="accordion-card">
+        <div class="title">
+            <h3>{{ title }}</h3>
+            <div class="expand">x</div>
+        </div>
+        <div class="content">
+            <p>{{ content }}</p>
+            <p v-if="link !== null && !link.isEmpty()">For more information, click <a :href="link">here</a>.</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+    import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
+    export default Vue.extend({
+        name: "Footer",
+        props: {
+            title: {required: true, type: String},
+            content: {required: true, type: String},
+            link: {required: false, type: String}
+        },
+        components: {
+            FontAwesomeIcon
+        },
+        computed: {
+
+        }
+    });
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
