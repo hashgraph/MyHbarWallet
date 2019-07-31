0019efa8cc5638b5339d6b905e7e71beb500e0f6
Chris Campbell <chris@launchbadge.com>
2019-07-31 15:37:21 -0700

    basic skeleton with animation

--- src/components/AccordionCard.vue
+++ src/components/AccordionCard.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="accordion-card">
-        <div class="title">
+        <div class="title" @click="toggle">
             <h3>{{ title }}</h3>
-            <div class="expand">x</div>
+            <div class="icon-container">
+                <transition name="flip" mode="out-in" enter-active-class="spin">
+                    <FontAwesomeIcon
+                        :key="expanded"
+                        size="lg"
+                        class="icon"
+                        :icon="icon"
+                    />
+                </transition>
+            </div>
         </div>
-        <div class="content">
-            <p>{{ content }}</p>
-            <p v-if="link !== null && !link.isEmpty()">
+        <div v-if="expanded" class="content-container">
+            <div class="content">{{ content }}</div>
+            <div v-if="link !== ''">
                 For more information, click <a :href="link">here</a>.
-            </p>
+            </div>
         </div>
     </div>
 </template>
@@ -19,17 +28,56 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 export default Vue.extend({
-    name: "Footer",
+    name: "AccordionCard",
     components: {
         FontAwesomeIcon
     },
     props: {
         title: { required: true, type: String },
         content: { required: true, type: String },
-        link: { required: false, type: String }
+        link: { required: false, type: String, default: "" }
     },
-    computed: {}
+    data: function() {
+        return {
+            expanded: false
+        };
+    },
+    computed: {
+        icon() {
+            if (this.expanded) {
+                return faPlus;
+            } else {
+                return faMinus;
+            }
+        }
+    },
+    methods: {
+        toggle() {
+            this.expanded = !this.expanded;
+        }
+    }
 });
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+@keyframes rotate {
+    from {
+        transform: rotate(0);
+    }
+
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+.spin {
+    animation: rotate;
+    animation-duration: 0.1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+
+    @media screen and (prefers-reduced-motion: reduce) {
+        animation: none;
+    }
+}
+</style>
