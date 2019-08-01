0ba4c691b027c8ec0d775974ca8f5181db065944
Daniel Akhterov <daniel@launchbadge.com>
2019-07-31 17:02:51 -0700

    Warning label

---
+++ src/components/Warning.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="warning">
+        <div class="warning-symbol">
+            ⚠️
+        </div>
+        <div class="text">
+            <div class="title">
+                {{ title }}
+            </div>
+            <slot></slot>
+            <!-- TODO: > Read: Using MEW Offline Component here -->
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "Warning",
+    props: {
+        title: { type: String, required: false, default: "" }
+    }
+});
+</script>
+
+<style scoped lang="postcss">
+.warning {
+    /* grid-template-columns: 60px 1fr; */
+    background-color: var(--color-sheer-peach);
+    display: flex;
+    padding: 20px;
+    width: 100%;
+}
+
+.warning-symbol {
+    flex-shrink: 0;
+    font-size: 30px;
+    height: 122px;
+    width: 60px;
+}
+
+.image {
+    height: 30px;
+    width: 30px;
+}
+
+.title {
+    font-size: 20px;
+    font-weight: 600;
+    margin-block-end: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-    <div class="home"></div>
+    <div class="home">
+        <Warning title="NOT RECOMMENDED">
+            This is not a recommended way to access your wallet. Due to the
+            sensitivity of the information involved, these options should only
+            be used in offline settings by experienced users.
+        </Warning>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Warning from "../components/Warning.vue";
 
 export default Vue.extend({
     name: "Home",
-    components: {}
+    components: {
+        Warning
+    }
 });
 </script>
 
