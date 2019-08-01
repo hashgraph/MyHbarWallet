44275b71ec51a7c9adddb6c9e591c1340ba13367
Chris Campbell <chris@launchbadge.com>
2019-08-01 10:04:34 -0700

    pull in customer support link

---
+++ src/components/CustomerSupportLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="CustomerSupportLink">
+        <img src="../assets/help-center.svg" /> Customer Support
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="postcss" scoped>
+.CustomerSupportLink {
+    color: var(--color-melbourne-cup);
+    cursor: pointer;
+    text-align: center;
+}
+</style>
