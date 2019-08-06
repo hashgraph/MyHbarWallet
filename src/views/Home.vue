b0a43162c6c4481ddc1974ed720343639684c1bb
Josh <josh@launchbadge.com>
2019-08-06 03:45:08 -0700

    wip -> home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,42 @@
 <template>
     <div class="home">
         <div class="top">
-            <HomeTileButtons />
+            <div class="page-container">
+                <div class="banner">
+                    <div class="banner-content">
+                        <h1>Hedera's</h1>
+                        <h1>Original Wallet</h1>
+                        MyHederaWallet is a free, client-side interface helping
+                        you interact with Hedera hashgraph. Our easy-to-use,
+                        open-source platform allows you to generate wallets,
+                        interact with smart contracts, and so much more.
+                    </div>
+                    <div class="banner-image"></div>
+                </div>
+                <HomeTileButtons />
+            </div>
         </div>
+        <div class="about">
+            <div class="page-container">
+                <div class="banner">
+                    <div class="banner-content">
+                        <h2>About MyHederaWallet</h2>
+                        <p>
+                            MyHederaWallet puts Hedera Hashgraph at your
+                            fingertips. Bacon ipsum dolor amet shank swine beef
+                            ribs kevin chicken, jowl prosciutto flank shankle
+                            tenderloin burgdoggen leberkas sausage pork belly.
+                            Bresaola flank beef meatball fatback ground round,
+                            pancetta jerky porchetta.
+                        </p>
+                    </div>
+                    <div class="banner-image"></div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom"></div>
         <FAQs />
+        <div class="community"></div>
     </div>
 </template>
 
@@ -24,19 +57,21 @@ export default Vue.extend({
 
 <style lang="postcss" scoped>
 .home {
-    display: flex;
-    flex-direction: column;
-
     @media (max-width: 414px) {
         align-items: stretch;
     }
 }
 
 .top {
+    background-color: white;
+}
+
+.page-container {
     align-items: center;
     align-self: center;
     display: flex;
     flex-direction: column;
+    margin: 0 auto;
     max-width: 1024px;
 
     @media (min-width: 1025px) {
@@ -53,5 +88,38 @@ export default Vue.extend({
         /* fixme: remove 80px when there is more content */
         padding: 80px 25px;
     }
+}
+
+.banner {
+    align-items: center;
+    align-self: center;
+    display: flex;
+    width: 100%;
+}
+
+.banner-content {
+    float: inline-start;
+    font-family: Montserrat, sans-serif;
+    font-size: 14px;
+    max-width: 50%;
+    text-align: start;
+
+    & > h1 {
+        color: var(--color-ghostlands-coal);
+        font-size: 45px;
+        font-weight: 700px;
+        letter-spacing: -1px;
+        line-height: 47px;
+    }
+}
+
+.banner-image {
+    display: flex;
+    flex: 1;
+}
+
+.about {
+    background-color: var(--color-yankees-blue);
+    color: white;
 }
 </style>
