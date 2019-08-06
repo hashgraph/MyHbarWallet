fe25bdf022d55bedd443a12607c71ffe07e08c9c
Chris Campbell <chris@launchbadge.com>
2019-08-05 17:41:32 -0700

    merge tests fixes

--- src/components/AccountTileButton.vue
+++ src/components/AccountTileButton.vue
@@ -103,6 +103,24 @@ export default Vue.extend({
         & .title,
         & .content,
         & .note {
+            /* stylelint-disable-next-line plugin/no-low-performance-animation-properties */
+            transition: none;
+        }
+    }
+
+    @media screen and (prefers-reduced-motion: reduce) {
+        & .title,
+        & .content,
+        & .note {
+            /* stylelint-disable-next-line plugin/no-low-performance-animation-properties */
+            transition: none;
+        }
+    }
+
+    @media screen and (prefers-reduced-motion: reduce) {
+        & .title,
+        & .content,
+        & .note {
             transition: none;
         }
     }
