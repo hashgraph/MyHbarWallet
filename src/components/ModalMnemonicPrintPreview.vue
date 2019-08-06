6af931ae94cc00c82475439acaf3a2e47ed43269
josh <josh@launchbadge.com>
2019-08-05 20:47:57 -0700

    better styling for the print preview modal

--- src/components/ModalMnemonicPrintPreview.vue
+++ src/components/ModalMnemonicPrintPreview.vue
@@ -1,30 +1,33 @@
 <template>
-    <Modal is-open title="Print Preview">
-        <template v-slot:content>
-            <div class="logo-contents">
-                <div class="logo">
-                    My<strong>Hedera</strong>Wallet |
-                    <span>Mnemonic Phrase</span>
-                </div>
-                <div class="support-email">
-                    <img
-                        alt=""
-                        class="icon"
-                        src="../assets/icon-bell.svg"
-                    />support@myhbarwallet.com
-                </div>
+    <Modal
+        :is-open="isOpen"
+        :large="true"
+        title="Print Preview"
+        @change="this.$listeners.change"
+    >
+        <div class="logo-contents">
+            <div class="logo">
+                My<strong>Hedera</strong>Wallet |
+                <span>Mnemonic Phrase</span>
             </div>
-            <div class="password-disclaimer">
-                <h3>
-                    Please Keep This Sheet in a Very Safe Place. It is your
-                    property!
-                </h3>
-                We <strong>CAN NOT</strong> change your password. Please
-                <strong>DO NOT FORGET</strong> to save your password. It is your
-                <strong>PRIVATE KEY</strong>. You will need this
-                <strong>Password + Mnemonic Phrase</strong> to access your
-                wallet.
+            <div class="support-email">
+                <img
+                    alt=""
+                    class="icon"
+                    src="../assets/icon-bell.svg"
+                />support@myhbarwallet.com
             </div>
+        </div>
+        <div class="password-disclaimer">
+            <h3>
+                Please Keep This Sheet in a Very Safe Place. It is your
+                property!
+            </h3>
+            We <strong>CAN NOT</strong> change your password. Please
+            <strong>DO NOT FORGET</strong> to save your password. It is your
+            <strong>PRIVATE KEY</strong>. You will need this
+            <strong>Password + Mnemonic Phrase</strong> to access your wallet.
+        </div>
 
             <MnemonicInput :editable="editable" :words="words" :value="value" />
             <div class="button-container">
@@ -46,40 +49,46 @@ export default Vue.extend({
         Modal,
         MnemonicInput
     },
-    data() {
-        return {
-            editable: false,
-            words: 12,
-            value: []
-        };
+    model: {
+        prop: "isOpen",
+        event: "change"
+    },
+    props: {
+        isOpen: { type: Boolean },
+        words: {
+            type: Array,
+            default: () => ["", "", "", "", "", "", "", "", "", "", "", ""]
+        }
     }
 });
 </script>
 
 <style scoped lang="postcss">
 .logo-contents {
+    align-items: center;
     display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
+    flex-direction: row;
+    flex-wrap: wrap;
+    font-size: 16px;
+    justify-content: space-between;
 }
 
 .logo {
-    font-size: 26px;
+    font-size: 24px;
 
     & > strong {
         font-weight: 600;
     }
 
     & > span {
-        color: #05c0a5;
+        color: var(--color-melbourne-cup);
         font-size: 14px;
         font-weight: 700;
     }
 }
 
 .support-email {
-    color: #506175;
-    font-size: 16px;
+    color: var(--color-basalt-grey);
     margin-inline-start: 5px;
 
     & > img {
@@ -88,6 +97,8 @@ export default Vue.extend({
 }
 
 .password-disclaimer {
+    color: var(--color-basalt-grey);
+    font-size: 14px;
     margin-block-end: 30px;
 
     & > strong {
@@ -96,6 +107,7 @@ export default Vue.extend({
     }
 
     & > h3 {
+        color: black;
         font-size: 16px;
         font-weight: 700;
     }
