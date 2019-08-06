8d56b11276616b9f4114728a23ec7655c3ecbbba
Chris Campbell <chris@launchbadge.com>
2019-08-06 15:41:32 -0700

    skeleton

---
+++ src/components/InfoTooltip.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="info-tooltip">
+        <MaterialDesignIcon :icon="icon" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import MaterialDesignIcon from "@/components/MaterialDesignIcon.vue";
+import { mdiExclamation } from "@mdi/js";
+
+export default Vue.extend({
+    name: "InfoTooltip",
+    components: {
+        MaterialDesignIcon
+    },
+    props: {
+        message: { type: String, required: true, default: "" }
+    },
+    computed: {
+        icon() {
+            return mdiExclamation;
+        }
+    }
+});
+</script>
+
+<style lang="postcss" scoped>
+.info-tooltip {
+    color: var(--color-china-blue);
+}
+</style>
