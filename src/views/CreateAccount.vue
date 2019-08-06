bcf6d9d1a8d4910eaf16a0a0411351f8372f2fd3
Chris Campbell <chris@launchbadge.com>
2019-08-06 10:26:12 -0700

    finish actual view: create account (not tabbed menu component though)

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,37 +1,67 @@
 <template>
-    <div class="create-account">
-        <div class="top">
-            <AccountTileButtons />
+    <div class="access-my-account">
+        <div class="wrap">
+            <div class="title-container">
+                <div class="title">Create A New Account</div>
+                <div class="subtitle">
+                    Already Have One?
+                    <router-link to="access-my-account">
+                        Access My Account
+                    </router-link>
+                </div>
+            </div>
+            <CreateTabbedMenu />
         </div>
         <FAQs />
     </div>
 </template>
-
 <script lang="ts">
 import Vue from "vue";
 import FAQs from "../components/FAQs.vue";
-import AccountTileButtons from "../components/AccountTileButtons.vue";
+// import CreateTabbedMenu from "@/components/CreateTabbedMenu.vue";
 
 export default Vue.extend({
     components: {
-        FAQs,
-        AccountTileButtons
+        FAQs
+        // CreateTabbedMenu
     },
     computed: {}
 });
 </script>
 
 <style lang="postcss" scoped>
-.create-account {
+a {
+    color: var(--color-melbourne-cup);
+    cursor: pointer;
+    text-decoration: none;
+}
+
+.access-my-account {
     display: flex;
     flex-direction: column;
 }
 
-.top {
+.wrap {
     align-items: center;
-    align-self: center;
     display: flex;
     flex-direction: column;
-    max-width: 1024px;
+    padding: 80px 0;
+}
+
+.title-container {
+    margin-block-end: 50px;
+    text-align: center;
+}
+
+.title {
+    font-size: 30px;
+    font-weight: 500;
+    margin-block-end: 15px;
+}
+
+.subtitle {
+    color: var(--color-china-blue);
+    font-size: 14px;
+    font-weight: 400;
 }
 </style>
