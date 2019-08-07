f3b2519b8de172414b83585bfa71477c5bff1cd7
Zachery Gyurkovitz <zach@launchbadge.com>
2019-08-07 16:15:23 -0700

    Add a Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="title-container">
+        <!-- todo: move this to a computed, it's being difficult with the  -->
+        <div class="title" :class="large ? 'lg' : ''">{{ title }}</div>
+        <div class="subtitle"><slot></slot></div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "Title",
+    props: {
+        title: { type: String, required: true },
+        large: { type: Boolean }
+    }
+});
+</script>
+
+<style lang="postcss" scoped>
+a {
+    color: var(--color-melbourne-cup);
+    cursor: pointer;
+    text-decoration: none;
+}
+
+.access-my-account {
+    display: flex;
+    flex-direction: column;
+}
+
+.wrap {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    padding: 80px 0;
+}
+
+.title-container {
+    margin-block-end: 50px;
+    text-align: center;
+}
+
+.title {
+    font-size: 30px;
+    font-weight: 500;
+    margin-block-end: 15px;
+}
+
+.title.lg {
+    font-size: 45px;
+}
+
+.subtitle {
+    color: var(--color-china-blue);
+    font-size: 14px;
+    font-weight: 400;
+}
+</style>
