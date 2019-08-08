c49a66a2feb2304bcc6c5cb2c352291c2cbc0505
Daniel Akhterov <daniel@launchbadge.com>
2019-08-08 15:06:21 -0700

    Update transaction fee css

--- src/components/SendTransfer.vue
+++ src/components/SendTransfer.vue
@@ -8,6 +8,8 @@
                     label="Amount"
                     type="number"
                     compact
+                    action="Entire Balance"
+                    @action="entireBalanceActionClick"
                 />
                 <div v-if="true" class="warning">Not enough Hbar to send.</div>
             </div>
@@ -17,37 +19,60 @@
                     v-model="toAccount"
                     placeholder="shard.realm.account"
                     validate-id
-                    label="To Accouunt"
+                    label="To Account"
                     compact
+                    can-copy
                 />
             </div>
 
             <div class="field">
-                <TextInput
-                    :value="maxFee"
-                    label="Maximum Transaction Fee"
-                    compact
-                />
+                <div class="label">
+                    Maximum Transaction Fee
+                </div>
+                <div class="fee-container">
+                    <div class="fee">
+                        {{ maxFee }}
+                    </div>
+                    <div class="conversion">
+                        {{ rate }}
+                    </div>
+                </div>
             </div>
         </InterfaceTile>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
 import TextInput from "../components/TextInput.vue";
 import InterfaceTile from "../components/InterfaceTile.vue";
+import { createComponent, value } from "vue-function-api";
 
-export default Vue.extend({
+export default createComponent({
     components: {
         TextInput,
         InterfaceTile
     },
-    data() {
+    setup(): {} {
+        const amount = value("0");
+        const toAccount = value("");
+        const maxFee = value("1,000,000 HBar");
+        const rate = value("Cost 1 HBar = $0.12");
+
+        function entireBalanceActionClick() {
+            console.log("entireBalanceActionClick");
+        }
+
+        function copyAccountActionClick() {
+            console.log("copyAccountActionClick");
+        }
+
         return {
-            amount: "0",
-            toAccount: "",
-            maxFee: "1,000,000 HBar"
+            amount,
+            toAccount,
+            maxFee,
+            rate,
+            entireBalanceActionClick,
+            copyAccountActionClick
         };
     }
 });
@@ -57,10 +82,28 @@ export default Vue.extend({
 .warning {
     color: var(--color-red);
     font-size: 14px;
-    margin-inline: 15px 7px;
+    margin-block-start: 7px;
+    margin-inline-start: 15px;
 }
 
 .field {
     margin-block-end: 30px;
+}
+
+.fee-container {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10px;
+    width: 100%;
+}
+
+.label {
+    cursor: pointer;
+    display: block;
+    font-size: 16px;
+    font-weight: 600;
+    margin-block-end: 13px;
+    padding: 0 10px;
 }
 </style>
--- src/views/InterfaceSendTransfer.vue
+++ src/views/InterfaceSendTransfer.vue
@@ -7,12 +7,9 @@
 <script lang="ts">
 import Vue from "vue";
 import SendTransfer from "../components/SendTransfer.vue";
-// import TextInput from "../components/TextInput.vue";
 
 export default Vue.extend({
     name: "InterfaceSendTransfer",
-    components: {
-        SendTransfer
-    }
+    components: { SendTransfer }
 });
 </script>
