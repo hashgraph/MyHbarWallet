109a036900cbe3ecdba968cffca46d534165eb13
Kristy <kristy@launchbadge.com>
2019-08-08 16:17:12 -0700

    Remove apparently unnecessary type declarations for clipboard

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -1,16 +1,5 @@
 // Type declarations for Clipboard API
 // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
-interface Clipboard {
-    writeText(text: string): Promise<void>;
-}
-
-interface NavigatorClipboard extends Navigator {
-    // Only available in a secure context.
-    readonly clipboard: Clipboard;
-}
-
-interface NavigatorExtended extends NavigatorClipboard {}
-
 export async function writeToClipboard(value: string) {
-    await (navigator as NavigatorExtended).clipboard!.writeText(value);
+    await navigator.clipboard.writeText(value);
 }
