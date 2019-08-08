a19681ced072cad64b0a13dfe8e7086e8d29771f
Kristy <kristy@launchbadge.com>
2019-08-08 16:25:09 -0700

    Add shiny things to TextArea and make it functional

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="input-container" :class="classObject">
+    <div class="input-container">
         <label>
             <span v-if="label" class="label">{{ label }}</span>
             <textarea
@@ -7,29 +7,94 @@
                 :placeholder="placeholder"
                 :tabindex="tabindex"
                 :value="value"
+                :rows="rows"
+                :class="{ resize: resizeable }"
                 @input="handleInput"
-            ></textarea>
+            />
         </label>
+        <MaterialDesignIcon
+            v-if="showValidation"
+            class="validation"
+            :class="{ valid }"
+            :icon="checkmark"
+            :size="18"
+        />
+        <div class="actions">
+            <div v-if="canClear" class="action" @click="onClickClear">
+                Clear
+            </div>
+            <div v-if="canCopy" class="action" @click="onClickCopy">
+                Copy
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { writeToClipboard } from "../clipboard";
+import { mdiCheckCircle } from "@mdi/js";
+import MaterialDesignIcon from "./MaterialDesignIcon.vue";
+import { createComponent, value, computed, PropType } from "vue-function-api";
 
-export default Vue.extend({
-    props: {
-        placeholder: { type: String, default: "" },
-        value: { type: String, default: "" },
-        label: { type: String, default: null },
-        tabindex: { type: String, default: null }
+interface Props {
+    placeholder: string;
+    value: string;
+    label: string;
+    tabindex: string;
+    canCopy: boolean;
+    canClear: boolean;
+    compact: boolean;
+    resizeable: boolean;
+    showValidation: boolean;
+    valid: boolean;
+}
+
+export default createComponent({
+    components: {
+        MaterialDesignIcon
     },
-    methods: {
-        focus() {
-            (this.$refs.input as HTMLTextAreaElement).focus();
-        },
-        handleInput(event: Event) {
-            this.$emit("input", (event.target as HTMLTextAreaElement).value);
+    // TODO: https://github.com/vuejs/vue-function-api/pull/16
+    props: {
+        placeholder: (String as unknown) as PropType<string>,
+        value: (String as unknown) as PropType<string>,
+        label: (String as unknown) as PropType<string>,
+        tabindex: (String as unknown) as PropType<string>,
+        canCopy: (Boolean as unknown) as PropType<boolean>,
+        canClear: (Boolean as unknown) as PropType<boolean>,
+        compact: (Boolean as unknown) as PropType<boolean>,
+        resizeable: (Boolean as unknown) as PropType<boolean>,
+        showValidation: (Boolean as unknown) as PropType<boolean>,
+        valid: (Boolean as unknown) as PropType<boolean>
+    },
+    setup(props: Props, context) {
+        const rows = computed(() => {
+            return props.compact ? 2 : 8;
+        });
+
+        const checkmark = computed(() => {
+            return mdiCheckCircle;
+        });
+
+        function handleInput(event: Event) {
+            context.emit("input", (event.target as HTMLTextAreaElement).value);
         }
+
+        function onClickClear() {
+            context.emit("input", "");
+        }
+
+        async function onClickCopy() {
+            await writeToClipboard(props.value);
+        }
+
+        return {
+            rows,
+            checkmark,
+            handleInput,
+            onClickClear,
+            onClickCopy
+        };
     }
 });
 </script>
@@ -46,17 +111,23 @@ textarea {
     border-radius: 4px;
     color: var(--color-washed-black);
     font-size: 14px;
-    min-height: 200px;
+    max-width: 100%;
+    min-width: 100%;
     outline: none;
     padding: 20px;
-    width: 100%;
+    resize: none;
 
     &:focus {
         border-color: var(--color-melbourne-cup);
     }
 
-    &:not(:only-child) {
-        padding-inline-end: 50px;
+    &.resize {
+        resize: vertical;
+    }
+
+    &::placeholder {
+        color: black;
+        opacity: 0.2;
     }
 }
 
@@ -64,7 +135,35 @@ textarea {
     display: block;
     font-size: 16px;
     font-weight: 600;
-    margin-block-end: 13px;
+    margin-block-end: 10px;
     padding: 0 8px;
+}
+
+.actions {
+    display: flex;
+    inset-block-start: 0;
+    inset-inline-end: 8px;
+    position: absolute;
+}
+
+.action {
+    color: var(--color-basalt-grey);
+    margin-inline-start: 20px;
+
+    &:hover,
+    &:focus {
+        cursor: pointer;
+    }
+}
+
+.validation {
+    color: var(--color-jupiter);
+    inset-block-end: 13px;
+    inset-inline-end: 16px;
+    position: absolute;
+
+    &.valid {
+        color: var(--color-melbourne-cup);
+    }
 }
 </style>
