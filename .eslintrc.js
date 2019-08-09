dc10fd2682572ff5119d23ba8b2cf82cb8635601
Ryan Leckey <ryan@launchbadge.com>
2019-08-09 14:57:13 -0700

    [eslint] constraint typescript usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,6 +57,34 @@ module.exports = {
 
         // Unicorn
         "unicorn/filename-case": false,
-        "unicorn/prevent-abbreviations": false
+        "unicorn/prevent-abbreviations": false,
+
+        // Typescript
+        "no-var": "error",
+        "prefer-const": "error",
+        "prefer-rest-params": "error",
+        "prefer-spread": "error",
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/ban-types": "error",
+        "@typescript-eslint/camelcase": "error",
+        "@typescript-eslint/class-name-casing": "error",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/explicit-member-accessibility": "error",
+        "@typescript-eslint/interface-name-prefix": "error",
+        "@typescript-eslint/member-delimiter-style": "error",
+        "@typescript-eslint/no-array-constructor": "error",
+        "@typescript-eslint/no-empty-interface": "error",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-inferrable-types": "error",
+        "@typescript-eslint/no-misused-new": "error",
+        "@typescript-eslint/no-namespace": "error",
+        "@typescript-eslint/no-non-null-assertion": "error",
+        "@typescript-eslint/no-parameter-properties": "error",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-use-before-define": "error",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/type-annotation-spacing": "error"
     }
 };
