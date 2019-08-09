7eba407bf71c1206f5c82beb451f120aa6062af4
Ryan Leckey <ryan@launchbadge.com>
2019-08-08 17:36:26 -0700

    Fix up Tooltip tests

---
+++ tests/unit/Tooltip.spec.ts
@@ -0,0 +1,71 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Tooltip from "@/components/Tooltip.vue";
+import { plugin as VueFunctionApi } from "vue-function-api";
+
+describe("Tooltip.vue", () => {
+    const localVue = createLocalVue();
+    localVue.use(VueFunctionApi);
+
+    it("opens tooltip on click for pinnable", () => {
+        const message = "Tooltip Text";
+        const wrapper = shallowMount(Tooltip, {
+            localVue,
+            propsData: {
+                message,
+                pinnable: true
+            },
+            slots: {
+                default: "<div class='component'>Hover Me</div>"
+            }
+        });
+
+        expect(wrapper.classes()).not.toContain("active");
+
+        wrapper.find(".slot-container").trigger("click");
+
+        expect(wrapper.classes()).toContain("active");
+        expect(wrapper.find(".message").text()).toMatch(message);
+    });
+
+    it("does not open tooltip on click for unpinnable", () => {
+        const wrapper = shallowMount(Tooltip, {
+            localVue,
+            propsData: {
+                message: "",
+                pinnable: false
+            },
+            slots: {
+                default: "<div class='component'>Hover Me</div>"
+            }
+        });
+
+        expect(wrapper.classes()).not.toContain("active");
+
+        wrapper.find(".slot-container").trigger("click");
+
+        expect(wrapper.classes()).not.toContain("active");
+    });
+
+    it("does open tooltip on mouseenter and close on mouseleave", () => {
+        const wrapper = shallowMount(Tooltip, {
+            localVue,
+            propsData: {
+                message: "",
+                pinnable: false
+            },
+            slots: {
+                default: "<div class='component'>Hover Me</div>"
+            }
+        });
+
+        expect(wrapper.classes()).not.toContain("active");
+
+        wrapper.find(".slot-container").trigger("mouseenter");
+
+        expect(wrapper.classes()).toContain("active");
+
+        wrapper.find(".slot-container").trigger("mouseleave");
+
+        expect(wrapper.classes()).not.toContain("active");
+    });
+});
