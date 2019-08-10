97b2ccc51526c0d6462b7c7f7c93624fcd5cf05c
Tim Schmidt <tim@launchbadge.com>
2019-08-09 19:25:14 -0700

    WIP Make Sign Message Component

--- src/components/SignMessage.vue
+++ src/components/SignMessage.vue
@@ -4,29 +4,17 @@
             <div class="title">Sign Message</div>
         </div>
         <div class="content">
-            <div class="description">
+            <div class="paragraph">
                 Include your nickname and where you use the nickname so someone
                 else cannot use it. Include a specific reason for the message so
                 it cannot be reused for a different purpose.
             </div>
-            <div class="text-input-label">Message</div>
-            <TextInput v-model="messageText" :multiline="true" />
-            <div v-if="!disableErr" class="validation-error">
-                The message field is required
-            </div>
-            <!-- TODO: Get messageText and do something with it? -->
+            <div class="text-area-label">Message</div>
+            <TextArea class="text-area" />
             <div class="bottom-content-container">
-                <Button
-                    class="sign-button"
-                    label="Sign"
-                    :disabled="disable"
-                    @click="$emit('sign', messageText)"
-                ></Button>
+                <button class="submit-button">Submit</button>
                 <div class="bottom-text">
-                    Have any issues?
-                    <router-link :to="{ name: 'help-center' }"
-                        >Help Center</router-link
-                    >
+                    Have any issues? <router-link>Help Center</router-link>
                 </div>
             </div>
         </div>
@@ -34,35 +22,12 @@
 </template>
 
 <script lang="ts">
-import TextInput from "../components/TextInput.vue";
-import Button from "../components/Button.vue";
-import { createComponent, watch, value } from "vue-function-api";
+import TextArea from "../components/TextArea.vue";
+import { createComponent } from "vue-function-api";
 
 export default createComponent({
     components: {
-        TextInput,
-        Button
-    },
-    setup() {
-        const messageText = value(null);
-        const disable = value(true);
-        const disableErr = value(true);
-
-        const classObject = () => {
-            if (disable) return "hidden";
-        };
-
-        watch(
-            () => messageText.value,
-            val => {
-                if (val) disable.value = false;
-                else disable.value = true;
-
-                if (val === "") disableErr.value = false;
-            }
-        );
-
-        return { messageText, disable, classObject, disableErr };
+        TextArea
     }
 });
 </script>
@@ -70,6 +35,7 @@ export default createComponent({
 <style lang="postcss" scoped>
 .sign-message {
     background-color: var(--color-white);
+    max-width: 1060.33px; /* remove */
 }
 
 .title {
@@ -89,13 +55,13 @@ export default createComponent({
     padding: 40px;
 }
 
-.description {
+.paragraph {
     color: var(--color-china-blue);
     margin-block-end: 40px;
     padding-inline: 8px;
 }
 
-.text-input-label {
+.text-area-label {
     color: var(--color-washed-black);
     font-size: 16px;
     font-weight: 600;
@@ -103,38 +69,22 @@ export default createComponent({
     padding-inline: 8px;
 }
 
+.text-area {
+    color: var(--color-washed-black);
+    margin-block-end: 30px;
+}
+
 .bottom-content-container {
     display: flex;
-    flex-flow: column wrap;
-    margin-block-start: 30px;
+    justify-content: center;
     margin-block-start: 60px;
 }
 
-.sign-button {
-    margin-inline: auto;
+.submit-button {
+    display: block;
 }
 
 .bottom-text {
-    color: var(--color-china-blue);
-    margin-block-start: 20px;
-    margin-inline: auto;
-}
-
-button:first-child {
-    min-width: 300px;
-}
-
-a {
-    color: var(--color-melbourne-cup);
-    text-decoration: none;
-
-    &:hover,
-    &:focus {
-        text-decoration: underline;
-    }
-}
-
-.validation-error {
-    color: var(--color-washed-black);
+    align-self: flex-end;
 }
 </style>
