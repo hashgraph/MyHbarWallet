6a63d15d06d86099d8352a0c3fe3738de086c748
Ryan Leckey <ryan@launchbadge.com>
2019-08-12 11:28:13 -0700

    Run formatter

--- tests/unit/InterfaceNavigation.spec.ts
+++ tests/unit/InterfaceNavigation.spec.ts
@@ -2,10 +2,9 @@ import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
 import InterfaceNavigationSection from "../../src/components/InterfaceNavigationSection.vue";
 import InterfaceNavigation from "../../src/components/InterfaceNavigation.vue";
 
-
 describe("InterfaceNavigation", () => {
     const wrapper = shallowMount(InterfaceNavigation);
     it("renders with children components", () => {
         expect(wrapper.find(InterfaceNavigationSection).exists()).toBeTruthy();
-    })
+    });
 });
