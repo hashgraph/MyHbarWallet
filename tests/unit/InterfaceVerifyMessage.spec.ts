f998d79c7b270803ed6aafa5480add49b2e65704
Kristy <kristy@launchbadge.com>
2019-08-13 11:27:10 -0700

    Fix a test for InterfaceVerifyMessage

--- tests/unit/InterfaceVerifyMessage.spec.ts
+++ tests/unit/InterfaceVerifyMessage.spec.ts
@@ -1,16 +1,22 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
 import InterfaceVerifyMessage from "@/views/InterfaceVerifyMessage.vue";
+import { plugin as VueFunctionApi } from "vue-function-api";
 
 describe("InterfaceVerifyMessage.vue", (): void => {
-    it("renders", (): void => {
-        const wrapper = shallowMount(InterfaceVerifyMessage);
+    const localVue = createLocalVue();
+    localVue.use(VueFunctionApi);
 
-        expect(wrapper.find(".verify-message")).toBeTruthy();
+    it("renders", (): void => {
+        expect.hasAssertions();
+        const wrapper = shallowMount(InterfaceVerifyMessage, { localVue });
+
+        expect(wrapper.find(".verify-message")).not.toBeNull();
     });
 
-    // it("renders a disabled button when signature is empty", (): void => {
-    //     const wrapper = shallowMount(InterfaceVerifyMessage);
-    //
-    //     expect(wrapper.find("button").attributes()["disabled"]).toBeTruthy();
-    // });
+    it("renders a disabled button when signature is empty", (): void => {
+        expect.hasAssertions();
+        const wrapper = mount(InterfaceVerifyMessage, { localVue });
+
+        expect(wrapper.find("button").attributes()["disabled"]).not.toBeNull();
+    });
 });
