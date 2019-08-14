6b608f438d6086116b32d113b30456ab6e60147a
Zachery Gyurkovitz <zach@launchbadge.com>
2019-08-14 10:11:17 -0700

    Add "Success Creating KeyPair" Modal

---
+++ tests/unit/ModalSuccessCreatingKeyPair.spec.ts
@@ -0,0 +1,23 @@
+import { shallowMount } from "@vue/test-utils";
+import ModalSuccessCreatingKeyPair from "@/components/ModalSuccessCreatingKeyPair.vue";
+import Modal from "@/components/Modal.vue";
+
+describe("ModalSuccessCreatingKeyPair.vue", (): void => {
+    it("renders", (): void => {
+        expect.assertions(2);
+        const wrapper = shallowMount(ModalSuccessCreatingKeyPair, {
+            propsData: {
+                isOpen: false
+            }
+        });
+
+        expect(wrapper.find(Modal).props().isOpen).toBe(false);
+
+        wrapper.setProps({ isOpen: true });
+        expect(wrapper.find(Modal).props().isOpen).toBe(true);
+    });
+
+    it("navigates", (): void => {
+        
+    })
+});
