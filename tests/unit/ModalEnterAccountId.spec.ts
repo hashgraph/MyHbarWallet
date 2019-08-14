29c64a946fb03eaab8a47a255a99b3c877cbe8d4
Daniel Akhterov <daniel@launchbadge.com>
2019-08-13 20:22:49 -0700

    Add the hedera js sdk to the project

--- tests/unit/ModalEnterAccountId.spec.ts
+++ tests/unit/ModalEnterAccountId.spec.ts
@@ -1,6 +1,6 @@
-import {createLocalVue, shallowMount} from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import ModalEnterAccountId from "@/components/ModalEnterAccountId.vue";
-import {plugin as VueFunctionApi} from "vue-function-api";
+import { plugin as VueFunctionApi } from "vue-function-api";
 
 describe("ModalEnterAccountId.vue", (): void => {
     const localVue = createLocalVue();
@@ -8,7 +8,7 @@ describe("ModalEnterAccountId.vue", (): void => {
 
     it("renders", (): void => {
         expect.assertions(1);
-        const state = { modalIsOpen: false, isBusy: false, account: ""};
+        const state = { modalIsOpen: false, isBusy: false, account: "" };
         const wrapper = shallowMount(ModalEnterAccountId, {
             localVue,
             propsData: {
