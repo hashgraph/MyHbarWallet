e609605e0bd027bc61935384d6f76b89a32d05b4
Kristy <kristy@launchbadge.com>
2019-08-14 17:37:20 -0700

    Make tests not print angery red text

--- tests/unit/InterfaceNavigation.spec.ts
+++ tests/unit/InterfaceNavigation.spec.ts
@@ -1,11 +1,15 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import InterfaceNavigationSection from "../../src/components/InterfaceNavigationSection.vue";
 import InterfaceNavigation from "../../src/components/InterfaceNavigation.vue";
+import { plugin as VueFunctionApi } from "vue-function-api";
 
 describe("InterfaceNavigation", (): void => {
+    const localVue = createLocalVue();
+    localVue.use(VueFunctionApi);
+
     it("renders with children components", (): void => {
         expect.assertions(1);
-        const wrapper = shallowMount(InterfaceNavigation);
+        const wrapper = shallowMount(InterfaceNavigation, { localVue });
         expect(wrapper.find(InterfaceNavigationSection).exists()).toBe(true);
     });
 });
