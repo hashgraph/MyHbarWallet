2d50b8a5f5bb377f4b8e56521e920a8d5e2c8353
Ryan Leckey <ryan@launchbadge.com>
2019-08-22 18:27:43 -0700

    Setup e2e tests with Nightwatch and test Error404

--- tests/e2e/specs/Error404.js
+++ tests/e2e/specs/Error404.js
@@ -1,11 +1,8 @@
 module.exports = {
-    "renders 404 when navigating to an unknown route"(browser) {
+    "renders 404 when navigating to an unknown route": browser => {
         browser
-            .url(
-                `${process.env.VUE_DEV_SERVER_URL}/not-a-real-path-in-this-app`
-            )
+            .url(`${process.env.VUE_DEV_SERVER_URL}not-a-real-path-in-the-app`)
             .waitForElementVisible(".error-404", 5000)
-            .assert.elementPresent(".hello")
             .assert.containsText(".error-code", "404")
             .end();
     }
