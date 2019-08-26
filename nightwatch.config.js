a07e9c10e2788f15aa0cf6d7edda646ac47c4d96
Ryan Leckey <ryan@launchbadge.com>
2019-08-26 12:21:02 -0700

    Add configuration for NightWatch

---
+++ nightwatch.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+    src_folders: ["tests/e2e/specs"],
+    output_folder: "tests/e2e/reports",
+    custom_assertions_path: ["tests/e2e/custom-assertions"],
+
+    selenium: {
+        start_process: true,
+        server_path: require("selenium-server").path,
+        host: "127.0.0.1",
+        port: 4444,
+        cli_args: {
+            "webdriver.chrome.driver": require("chromedriver").path
+        }
+    },
+
+    test_settings: {
+        default: {
+            selenium_port: 4444,
+            selenium_host: "localhost",
+            silent: true
+        },
+
+        chrome: {
+            desiredCapabilities: {
+                browserName: "chrome",
+                javascriptEnabled: true,
+                acceptSslCerts: true
+            }
+        }
+    }
+};
