0d96d3163b9be758c22f01a66f2c7b70700274de
Ryan Leckey <ryan@launchbadge.com>
2019-08-27 14:22:50 -0700

    Allow TypeScript non-null assertion (val!)

--- .eslintrc.js
+++ .eslintrc.js
@@ -94,7 +94,6 @@ module.exports = {
         "@typescript-eslint/no-inferrable-types": "error",
         "@typescript-eslint/no-misused-new": "error",
         "@typescript-eslint/no-namespace": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
         "@typescript-eslint/no-parameter-properties": "error",
         "@typescript-eslint/no-unused-vars": "warn",
         "@typescript-eslint/no-use-before-define": "error",
