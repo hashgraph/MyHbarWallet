637e247dda7a94ed08d9b51f0c172a7dfdca0ff4
Kristy <kristy@launchbadge.com>
2019-08-27 12:04:53 -0700

    Fix some undefinedness and trim mnemonic input

--- src/components/ModalAccessByPhrase.vue
+++ src/components/ModalAccessByPhrase.vue
@@ -69,7 +69,7 @@ export default createComponent({
     props: {
         state: { type: Object, required: true } as PropOptions<State>
     },
-    setup(props, context) {
+    setup(props: { state: State }, context) {
         function handleModalChangeIsOpen(isOpen: boolean) {
             context.emit("change", { ...props.state, modalIsOpen: isOpen });
         }
@@ -80,7 +80,7 @@ export default createComponent({
         const areFieldsFilled = computed(() => {
             if (props.state.words.length == 24) {
                 for (const word of props.state.words) {
-                    if (word == "" || word == null) {
+                    if (!word || word.length === 0) {
                         return false;
                     }
                 }
