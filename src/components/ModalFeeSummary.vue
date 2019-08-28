eb8e30cc4ef4ed03a320da698040b8c46fdff099
Chris Campbell <chris@launchbadge.com>
2019-08-27 19:05:14 -0700

    WIP: ModalFeeSummary

---
+++ src/components/ModalFeeSummary.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="modal-fee-summary">
+        <Modal />
+    </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from "@vue/composition-api";
+import Modal from "../components/Modal.vue";
+
+export default createComponent({
+    components: {
+        Modal
+    }
+});
+</script>
+
+<style lang="postcss" scoped>
+.modal-fee-summary {
+    background-color: pink;
+}
+</style>
