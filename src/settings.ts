8cef159b53af59b31161480732d238b807f98d86
Ryan Leckey <ryan@launchbadge.com>
2019-09-03 22:00:17 -0700

    Allow network to be configured on deploy

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,27 @@
-export default {
-    network: {
-        // Mainnet
-        // proxy: "https://mainnet.grpc-web.myhederawallet.com",
-        // address: "35.237.200.180:50211",
-        // node: "0.0.3",
-        // name: "mainnet",
+interface NetworkSettings {
+    proxy: string;
+    address: string;
+    node: string;
+    name: string;
+}
 
-        // Testnet
+const availableNetworks: { [key: string]: NetworkSettings } = {
+    testnet: {
         proxy: "https://grpc-web.myhederawallet.com",
         address: "0.testnet.hedera.com:50211",
         node: "0.0.3",
         name: "testnet"
+    },
+    mainnet: {
+        proxy: "https://mainnet.grpc-web.myhederawallet.com",
+        address: "35.237.200.180:50211",
+        node: "0.0.3",
+        name: "mainnet",
     }
+}
+
+const selectedNetwork = process.env.HEDERA_NETWORK || "testnet";
+
+export default {
+    network: availableNetworks[selectedNetwork]!,
 };
