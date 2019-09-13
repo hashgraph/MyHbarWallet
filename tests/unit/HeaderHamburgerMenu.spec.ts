91455bd63bed9a7bec8176c8b85c4dffc0a4e424
Ryan Leckey <ryan@launchbadge.com>
2019-09-13 11:07:46 -0700

    Fix HeaderHamburgerMenu test (not using VueRouter)

--- tests/unit/HeaderHamburgerMenu.spec.ts
+++ tests/unit/HeaderHamburgerMenu.spec.ts
@@ -1,37 +1,38 @@
 import { createLocalVue, mount } from "@vue/test-utils";
 import VueCompositionApi from "@vue/composition-api";
+import VueRouter from "vue-router";
 import HeaderHamburgerMenu from "../../src/components/HeaderHamburgerMenu.vue";
+import router from "../../src/router";
 
 describe("HeaderHamburgerMenu.vue", (): void => {
     const localVue = createLocalVue();
     localVue.use(VueCompositionApi);
+    localVue.use(VueRouter);
 
     it("renders", (): void => {
         expect.assertions(1);
         const wrapper = mount(HeaderHamburgerMenu, {
             localVue,
+            router,
             propsData: {
                 isOpen: true
             }
         });
         expect(wrapper).toMatchInlineSnapshot(`
             <nav class="nav-open">
-              <!---->
-              <router-link to="/" class="link-block">
+              <!----> <a href="/" class="link-block router-link-exact-active router-link-active">
                 <div class="link">Home</div> <svg width="24" height="24" viewBox="0 0 24 24" class="icon">
                   <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
                 </svg>
-              </router-link>
-              <router-link to="[object Object]" class="link-block">
+              </a> <a href="/#about" class="link-block">
                 <div class="link">About</div> <svg width="24" height="24" viewBox="0 0 24 24" class="icon">
                   <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
                 </svg>
-              </router-link>
-              <router-link to="[object Object]" class="link-block">
+              </a> <a href="/#faqs" class="link-block">
                 <div class="link">FAQs</div> <svg width="24" height="24" viewBox="0 0 24 24" class="icon">
                   <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
                 </svg>
-              </router-link>
+              </a>
               <div class="logout-container">
                 <!---->
               </div>
