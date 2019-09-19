0c7f0cf41a93dcc029a4669deecf1fda9832c1d8
Chris Campbell <chris@launchbadge.com>
2019-09-19 11:06:29 -0700

    Separate Hbar / USD balance rendering and update e2e tests to wait for usd balance to be visible

--- tests/e2e/specs/Access/AccessAccountWithMnemonicPhrase.js
+++ tests/e2e/specs/Access/AccessAccountWithMnemonicPhrase.js
@@ -48,6 +48,7 @@ module.exports = {
             .text.to.match(
                 /^([1-9]\d{0,2}(?:,\d{3})*(\.\d{1,9})?|0?\.(?=.*[1-9])\d{1,9}) ℏ$/
             );
+        browser.waitForElementVisible(".interface .balance .usd-balance", 5000);
         browser.expect
             .element(".interface .balance .usd-balance")
             .text.to.match(
