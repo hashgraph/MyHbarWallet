0787ea7c9965271d616124902383888f310809a8
Ryan Leckey <ryan@launchbadge.com>
2019-10-08 17:19:02 -0700

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,8 @@ module.exports = {
     rules: {
         // v-html is used for rich text formatting for i18n
         "vue/no-v-html": "off",
-        // lints that were added later and should eventually be fixed and removed here
-        // so they are defaulted back to error
-        "unicorn/consistent-function-scoping": "warn",
-        "unicorn/expiring-todo-comments": "warn"
+
+        // This rule has many false positives in this codebase
+        "require-atomic-updates": "off"
     }
 };
