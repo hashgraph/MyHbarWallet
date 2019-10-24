14046dc3a6e3b39abca06b720ad3527b0415b84a
austins <austin.s@launchbadge.com>
2019-10-24 12:00:21 -0700

    Added warning text for when users attempt to access hardware wallets when they aren't on chrome

--- src/components/Warning.vue
+++ src/components/Warning.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="warning">
-        <Notice :title="$t('warning.title')" :symbol="mdiAlertOutline">
-            {{ $t("warning.thisIsNotARecommendedWayToAccessYourWallet") }}
-        </Notice>
+        <Notice :title="warningDisplay.title" :symbol="mdiAlertOutline">{{
+            warningDisplay.body
+        }}</Notice>
     </div>
 </template>
 
 <script lang="ts">
-import { createComponent } from "@vue/composition-api";
+import { createComponent, computed, SetupContext } from "@vue/composition-api";
 import { mdiAlertOutline } from "@mdi/js";
 import Notice from "../components/Notice.vue";
 
@@ -15,9 +15,35 @@ export default createComponent({
     components: {
         Notice
     },
-    setup() {
+    props: {
+        whichModal: { type: String, required: true }
+    },
+    setup({ whichModal }, context: SetupContext) {
+        const warningDisplay = computed(() => {
+            if (whichModal == "hardware") {
+                return {
+                    title: context.root
+                        .$t("warning.browserWarningTitle")
+                        .toString(),
+                    body: context.root
+                        .$t("warning.browserWarningBody")
+                        .toString()
+                };
+            } else {
+                return {
+                    title: context.root.$t("warning.title").toString(),
+                    body: context.root
+                        .$t(
+                            "warning.thisIsNotARecommendedWayToAccessYourWallet"
+                        )
+                        .toString()
+                };
+            }
+        });
+
         return {
-            mdiAlertOutline
+            mdiAlertOutline,
+            warningDisplay
         };
     }
 });
