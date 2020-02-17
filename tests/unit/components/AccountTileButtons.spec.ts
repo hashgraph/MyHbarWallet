7f04190f956ca4f30d9b00eb254fc1d41d69b1c3
Ryan Leckey <ryan@launchbadge.com>
2020-02-16 20:21:49 -0800

    Enable Hardware Button (w/Ledger)

--- tests/unit/components/AccountTileButtons.spec.ts
+++ tests/unit/components/AccountTileButtons.spec.ts
@@ -19,21 +19,16 @@ describe("AccountTileButtons.vue", (): void => {
 
         expect(wrapper).toMatchInlineSnapshot(`
             <div class="tile-grid">
-              <div class="ribbon-container">
-                <div class="coming-soon">
-                  Coming Soon!
-                </div>
-                <div class="account-tile-button disabled"><img src="" class="tile-image">
-                  <div class="text">
-                    <div class="title">
-                      Hardware
-                    </div>
-                    <div class="content">
-                      Support for hardware wallets in development
-                    </div>
-                    <div class="note">
+              <div class="account-tile-button"><img src="" class="tile-image">
+                <div class="text">
+                  <div class="title">
+                    Hardware
+                  </div>
+                  <div class="content">
+                    Support for hardware wallets in development
+                  </div>
+                  <div class="note">
 
-                    </div>
                   </div>
                 </div>
               </div>
