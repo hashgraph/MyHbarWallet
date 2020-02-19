e8ab970c9ec04132d36b043d2cc9cad0f6c08628
Chris Campbell <chris@launchbadge.com>
2020-02-19 12:42:20 -0800

    Add External Link Icon

--- src/components/TransactionLink.vue
+++ src/components/TransactionLink.vue
@@ -1,18 +1,26 @@
 <template>
-    <a
-        class="transaction-link"
-        :href="link"
-        target="_blank"
-    ><slot>View on Kabuto</slot></a>
+    <div class="transaction-link">
+        <a
+            :href="link"
+            target="_blank"
+        ><slot>View on Kabuto</slot></a>
+        &nbsp;
+        <MaterialDesignIcon
+            size="18"
+            :icon="mdiOpenInNew"
+        />
+    </div>
 </template>
 <script lang="ts">
 import { createComponent, computed } from "@vue/composition-api";
 import Button from "../components/Button.vue";
+import MaterialDesignIcon from "../components/MaterialDesignIcon.vue";
 import { getters } from "../store";
+import { mdiOpenInNew } from "@mdi/js";
 
 export default createComponent({
     props: { id: String },
-    components: { Button },
+    components: { Button, MaterialDesignIcon },
     setup(props) {
         const link = computed(() => {
             const network = getters.GET_NETWORK();
@@ -23,12 +31,14 @@ export default createComponent({
             return "";
         });
 
-        return { link };
+        return { link, mdiOpenInNew };
     }
 });
 </script>
 <style scoped lang="postcss">
 .transaction-link {
-    color: var(--color-california-orange);
+    & :first-child {
+        color: var(--color-california-orange);
+    }
 }
 </style>
