2d61c10f87224548e0b368e04be49651616b4af1
Tim <tim@launchbadge.com>
2020-02-20 12:13:26 -0800

    fix snaps

--- tests/unit/components/ModalUploadProgress.spec.ts
+++ tests/unit/components/ModalUploadProgress.spec.ts
@@ -244,15 +244,13 @@ describe("ModalUploadProgress.vue", (): void => {
                       <div class="upload-text">
                         Your file did not finish uploading
                       </div>
-                      <div class="upload-subtext">
+                      <div class="upload-subtext-try-again">
                         Try again?
                       </div>
                       <div class="visual-container"><svg width="120" height="120" viewBox="0 0 24 24" class="icon">
                           <path d="M14 2H6C4.89 2 4 2.9 4 4V20C4 21.11 4.89 22 6 22H18C19.11 22 20 21.11 20 20V8L14 2M18 20H6V4H13V9H18V20M11.95 15.46L14.08 17.58L12.66 19L10.54 16.87L8.42 19L7 17.58L9.13 15.46L7 13.34L8.42 11.92L10.54 14.05L12.66 11.92L14.08 13.34L11.95 15.46Z"></path>
                         </svg></div>
-                      <div class="progress-text">
-                        90.00%
-                      </div>
+                      <!---->
                       <div class="button-container"><button type="submit" class="cancel-button outline danger">
                           <!----> <span>Cancel</span>
                           <!----></button> <button type="submit" class="retry-button">
