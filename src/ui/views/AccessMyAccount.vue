2bdd5fb9990fb6869c08d3e55172d89491cf5721
Chris Campbell <cacampbell@ucdavis.edu>
2020-04-01 12:48:37 -0700

    remove debugger

--- src/ui/views/AccessMyAccount.vue
+++ src/ui/views/AccessMyAccount.vue
@@ -350,7 +350,6 @@ export default defineComponent({
             }
 
             try {
-                debugger;
                 await actions.logIn(account, state.wallet!, getters.currentNetwork());
                 state.modalEnterAccountIdState.isOpen = false;
                 Vue.nextTick(() => mutations.navigateToInterface());
