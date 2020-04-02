aea753c92f674492c1ef259188a9963add983784
austins <austin.s@launchbadge.com>
2020-04-02 14:36:31 -0700

    re-did pr, had to abandon old branch because it was horribly outdated.

---
+++ src/ui/components/FlatButton.vue
@@ -0,0 +1,52 @@
+<template>
+    <button
+        type="button"
+        :disabled="disabled"
+        v-on="disabled ? null : $listeners"
+    >
+        <MaterialDesignIcon :icon="icon" />
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+import MaterialDesignIcon from "./MaterialDesignIcon.vue";
+
+export default defineComponent({
+    components: { MaterialDesignIcon },
+    props: {
+        disabled: { type: Boolean },
+        icon: { type: String }
+    },
+    setup(props) {
+        return { props };
+    }
+});
+</script>
+
+<style scoped lang="postcss">
+button {
+    border: 2px solid transparent;
+    cursor: pointer;
+    margin: 0 0 0 4px;
+    outline: none;
+    text-align: center;
+
+    &:disabled {
+        background-color: var(--color-basalt-grey);
+        border-color: var(--color-basalt-grey);
+        cursor: default;
+    }
+
+    &:hover:not(:disabled),
+    &:focus:not(:disabled) {
+        border: 2px solid var(--color-melbourne-cup);
+        border-radius: 4px;
+    }
+
+    &:active:not(:disabled) {
+        transform: scale(0.85);
+    }
+}
+</style>
