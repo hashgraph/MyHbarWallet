9b38ab2649f6f01b272c4829b94de4ab015a2421
Chris Campbell <chris@launchbadge.com>
2021-04-02 16:34:49 -0700

    fix: modalaccessbyphrase spec, just make it disabled

--- tests/unit/components/ModalAccessByPhrase.spec.ts
+++ tests/unit/components/ModalAccessByPhrase.spec.ts
@@ -42,7 +42,7 @@ describe("ModalAccessByPhrase.vue", (): void => {
             "coast"
         ],
         isBusy: true,
-        isValid: true,
+        isValid: false,
         password: ""
     };
 
@@ -144,7 +144,7 @@ describe("ModalAccessByPhrase.vue", (): void => {
                   </div>
                 </transition-stub>
               </div>
-              </div> <button type="submit" class="continue-btn busy"><svg width="24" height="24" viewBox="0 0 24 24" class="spinner mdi-spin">
+              </div> <button type="submit" disabled="disabled" class="continue-btn busy"><svg width="24" height="24" viewBox="0 0 24 24" class="spinner mdi-spin">
                   <path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z"></path>
                 </svg>
                 <!---->
