19eccf9c07562383f7f970711f42d963b2761a3d
Chris Campbell <chris@launchbadge.com>
2021-07-08 16:32:38 -0700

    fix/chore: remove nodeHID package, vite cannot handle it

--- @types/images.d.ts
+++ @types/images.d.ts
@@ -1,5 +1,4 @@
 declare module "*.gif" {
-<<<<<<< HEAD
   const src: string;
   export default src;
 }
@@ -27,33 +26,4 @@ declare module "*.webp" {
 declare module "*.svg" {
   const src: string;
   export default src;
-=======
-    const src: string;
-    export default src;
-}
-
-declare module "*.jpg" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.jpeg" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.png" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.webp" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.svg" {
-    const src: string;
-    export default src;
->>>>>>> f34059c7 (feat: 0.6.0)
 }
