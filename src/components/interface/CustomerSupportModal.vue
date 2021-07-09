f684ab6456e329a08b4cce652b3f633d64428a86
Mason Meier <masonmeier@pop-os.localdomain>
2021-07-09 14:42:32 -0700

    fully implemented customer support button in footer as well as modal. Tested and working.
    Signed-off-by: Mason Meier <masonmeier@pop-os.localdomain>

---
+++ src/components/interface/CustomerSupportModal.vue
@@ -0,0 +1,180 @@
+//src/ui/components/ModalCustomerService.vue
+<template>
+    <teleport to="#modals">
+        <Modal
+            :is-visible="isVisible"
+            :title="$t('ModalCustomerService.title')"
+            @close="handleClose"
+        >
+                <form
+                    class="modal-issue-information"
+                    @submit.prevent="handleSubmit"
+                >
+                    <TextInput
+                        v-if="!hasBrowser"
+                        v-model="state.browser"
+                        class="issue-item"
+                        :placeholder="$t('ModalCustomerService.browser')"
+                    />
+                    <TextInput
+                        v-if="!hasPlatform"
+                        v-model="state.platform"
+                        class="issue-item"
+                        :placeholder="$t('ModalCustomerService.operatingSystem')"
+                    />
+                    <TextInput
+                        v-if="!hasDevice"
+                        v-model="state.device"
+                        class="issue-item"
+                        :placeholder="
+                            $t('ModalCustomerService.deviceOrWalletIfAny')
+                        "
+                    />
+                    <TextInput
+                        v-if="!hasAccountId"
+                        v-model="state.accountId"
+                        class="issue-item"
+                        :placeholder="$t('ModalCustomerService.accountIdIfAny')"
+                    />
+                    <TextInput
+                        v-if="!hasUrl"
+                        v-model="state.url"
+                        class="issue-item"
+                        :placeholder="$t('ModalCustomerService.url')"
+                    />
+                    <TextInput
+                        ref="input"
+                        v-model="state.description"
+                        multiline
+                        class="issue-item"
+                        :placeholder="$t('ModalCustomerService.describeTheIssue')"
+                        resizable
+                    />
+                    <Button
+                        :label="$t('common.send')"
+                        class="send-button"
+                        :compact="true"
+                    >{{$t('Common.send')}}
+                    </Button>
+                </form>
+        </Modal>
+    </teleport>
+</template>
+<script lang="ts">
+import platform from "platform";
+import { computed, defineComponent, reactive, ref, watch } from "vue";
+import { useStore } from "../../store";
+import { formatBuild, formatSupportLink } from "../../utils/format";
+import Modal from "./Modal.vue";
+import Button from "../base/Button.vue";
+import TextInput from "../base/TextInput.vue";
+import { useRouter, useRoute } from "vue-router";
+
+// from vite.config.js
+declare const __APP_VERSION__: string;
+declare const __APP_LAST_COMMIT_SHORT_HASH__: string;
+
+export default defineComponent({
+    name: "ModalCustomerService",
+    components: {
+        Button,
+        Modal,
+        TextInput
+    },
+    setup() {
+        const store = useStore();
+        const input = ref<HTMLInputElement | null>(null);
+        const router = useRouter();
+        const route = useRoute()
+        const appVersion = __APP_VERSION__;
+        const appLastCommitShortHash = __APP_LAST_COMMIT_SHORT_HASH__;
+        
+        const account = computed(() => store.accountId != null ?
+            store.accountId :
+            null);
+
+        const accountId: any = computed(() => {
+            if (account.value != null) {
+                return accountId.toString()
+            }
+            return null;
+        });
+
+        const browser = computed(() => {
+            const name = platform.name;
+            const version = platform.version;
+            return formatBuild(name ?? "", version);
+        });
+
+        const device = computed(() => {
+            const type = platform.product;
+            const model = platform.version;
+            return formatBuild(type, model);
+        });
+
+        const state = reactive({
+            platform: platform.os || "",
+            browser: browser.value || "",
+            url: route.name?.toString() ?? "",
+            description: "",
+            device: device.value || "",
+            version: appVersion + appLastCommitShortHash,
+            accountId: accountId.value || ""
+        });
+
+        const sendLink = computed(() => formatSupportLink(
+            state.url,
+            state.platform,
+            state.browser,
+            state.device,
+            state.version,
+            state.accountId,
+            state.description
+        ));
+
+        function handleSubmit(): void {
+            window.open(sendLink.value);
+            store.setContactSupportOpen(false);
+            clearForms();
+        }
+
+        function clearForms(): void {
+            state.device = "";
+            state.accountId = "";
+            state.description = "";
+        }
+        // When the route is updated, reset the path value
+        watch(
+            () => router.currentRoute,
+            () => {
+                if (router.currentRoute === undefined) return "unkown";
+                state.url = router.currentRoute?.toString() ?? ""
+                return null;
+            }
+        );
+
+        const isVisible = computed(() => {
+            return store.contactSupport
+        });
+
+        function handleClose(): void {
+            store.setContactSupportOpen(false);
+            clearForms();
+        }
+
+        return {
+            hasAccountId: accountId.value != null,
+            hasPlatform: platform.name!= null,
+            hasBrowser: browser.value != null,
+            hasUrl: state.url != null,
+            hasDevice: device.value != null,
+            state,
+            sendLink,
+            handleSubmit,
+            handleClose,
+            input,
+            isVisible,
+        };
+    }
+});
+</script>
