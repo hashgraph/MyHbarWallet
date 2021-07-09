3501354f56d78fd2aa27455c5cf1f89e845ed787
Mason Meier <masonmeier@pop-os.localdomain>
2021-07-09 14:42:32 -0700

    fully implemented customer support button in footer as well as modal. Tested and working.
    Signed-off-by: Mason Meier <masonmeier@pop-os.localdomain>

---
+++ src/components/interface/CustomerSupportButton.vue
@@ -0,0 +1,37 @@
+//this component is a wrapper for the customer support button
+<template>
+    <div>
+        <Button
+          class="hover:opacity-50 hover:no-underline"
+          @click="handleButtonClick"
+        >
+          {{ $t("CustomerSupport.button") }}
+        </Button>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from "../../store";
+import Button from '../base/Button.vue';
+import CustomerSupportModal from './CustomerSupportModal.vue';
+
+export default defineComponent({
+    name: "CustomerSupportButton",
+    components: {
+      Button,
+      CustomerSupportModal
+    },
+    setup() {
+        const store = useStore();
+
+        function handleButtonClick(): void {
+          store.setContactSupportOpen(true);
+        }
+
+        return {
+            handleButtonClick
+        };
+    },
+});
+</script>
+
