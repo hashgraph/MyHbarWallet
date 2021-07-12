69e4ecc39dd5b2f5d843488125394001457ed17c
masonmeier <57198981+masonmeier@users.noreply.github.com>
2021-07-12 14:03:25 -0700

    Added text input limitations to customer support modal (#451)
    Signed-off-by: Mason Meier <masonmeier@pop-os.localdomain>
    
    Co-authored-by: Mason Meier <masonmeier@pop-os.localdomain>

--- src/components/interface/CustomerSupportModal.vue
+++ src/components/interface/CustomerSupportModal.vue
@@ -14,48 +14,50 @@
           v-if="!hasBrowser"
           v-model="state.browser"
           class="issue-item"
+          :max-length="50"
           :placeholder="$t('ModalCustomerService.browser')"
         />
-
         <TextInput
           v-if="!hasPlatform"
           :model-value="state.platform.toString()"
           class="issue-item"
+          :max-length="50"
           :placeholder="$t('ModalCustomerService.operatingSystem')"
         />
-
         <TextInput
           v-if="!hasDevice"
           v-model="state.device"
           class="issue-item"
+          :max-length="50"
           :placeholder="
             $t('ModalCustomerService.deviceOrWalletIfAny')
           "
         />
-
         <TextInput
           v-if="!hasAccountId"
           v-model="state.accountId"
           class="issue-item"
+          :max-length="50"
           :placeholder="$t('ModalCustomerService.accountIdIfAny')"
         />
-
         <TextInput
           v-if="!hasUrl"
           v-model="state.url"
           class="issue-item"
+          :max-length="50"
           :placeholder="$t('ModalCustomerService.url')"
         />
-
+        <br/>
         <TextInput
           ref="input"
           v-model="state.description"
           multiline
+          :max-length="1000"
+          char-counter
           class="issue-item"
           :placeholder="$t('ModalCustomerService.describeTheIssue')"
           resizable
         />
-
         <Button
           class="w-full p-2"
           color="green"
@@ -67,23 +69,18 @@
     </Modal>
   </teleport>
 </template>
-
 <script lang="ts">
 import platform from "platform";
 import { computed, defineComponent, reactive, ref, watch } from "vue";
 import { useRouter, useRoute } from "vue-router";
-
 import { useStore } from "../../store";
 import { formatBuild, formatSupportLink } from "../../utils/format";
 import Button from "../base/Button.vue";
 import TextInput from "../base/TextInput.vue";
-
 import Modal from "./Modal.vue";
-
 // from vite.config.js
 declare const __APP_VERSION__: string;
 declare const __APP_LAST_COMMIT_SHORT_HASH__: string;
-
 export default defineComponent({
     name: "ModalCustomerService",
     components: {
@@ -98,30 +95,25 @@ export default defineComponent({
         const route = useRoute()
         const appVersion = __APP_VERSION__;
         const appLastCommitShortHash = __APP_LAST_COMMIT_SHORT_HASH__;
-        
         const account = computed(() => store.accountId != null ?
             store.accountId :
             null);
-
         const accountId = computed(() => {
             if (account.value != null) {
                 return account.value.toString()
             }
             return null;
         });
-
         const browser = computed(() => {
             const name = platform.name;
             const version = platform.version;
             return formatBuild(name ?? "", version);
         });
-
         const device = computed(() => {
             const type = platform.product;
             const model = platform.version;
             return formatBuild(type, model);
         });
-
         const state = reactive({
             platform: platform.os || "",
             browser: browser.value || "",
@@ -131,7 +123,6 @@ export default defineComponent({
             version: appVersion + appLastCommitShortHash,
             accountId: accountId.value || ""
         });
-
         const sendLink = computed(() => formatSupportLink(
             state.url,
             state.platform,
@@ -141,19 +132,16 @@ export default defineComponent({
             state.accountId,
             state.description
         ));
-
         function handleSubmit(): void {
             window.open(sendLink.value);
             store.setContactSupportOpen(false);
             clearForm();
         }
-
         function clearForm(): void {
             state.device = "";
             state.accountId = "";
             state.description = "";
         }
-
         // When the route is updated, reset the path value
         watch(
             () => router.currentRoute,
@@ -163,16 +151,13 @@ export default defineComponent({
                 return null;
             }
         );
-
         const isVisible = computed(() => {
             return store.contactSupport
         });
-
         function handleClose(): void {
             store.setContactSupportOpen(false);
             clearForm();
         }
-
         return {
             hasAccountId: accountId.value != null,
             hasPlatform: platform.name!= null,
