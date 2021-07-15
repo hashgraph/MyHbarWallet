39eac2f63df8c5996f0fa6748fcd65d456819285
Mason Meier <masonmeier@pop-os.localdomain>
2021-07-15 14:58:03 -0700

    Removed switches for send page memo/max fee amount. Fixed Styling.
    Signed-off-by: Mason Meier <masonmeier@pop-os.localdomain>

--- src/pages/interface/Send.vue
+++ src/pages/interface/Send.vue
@@ -3,7 +3,6 @@
     title="Send"
     parent="home"
   />
-
   <div
     class="pb-10 mt-8 font-medium border-b text-carbon border-cerebral-grey dark:border-midnight-express"
   >
@@ -16,16 +15,14 @@
         >
           {{ $t("InterfaceHomeSend.section1.header1") }}
         </div>
-
         <div
           v-else
           class="mb-2 dark:text-silver-polish"
         >
           {{ $t("InterfaceTransactionDetails.transfers") }}
         </div>
-
         <div
-          class=" 
+          class="
                   p-4
                   font-medium
                   bg-white
@@ -44,13 +41,11 @@
           />
         </div>
       </div>
-
       <div class="w-1/2 p-4 mt-4 mb-2 md:p-0">
         <OptionalMemo
           v-model="state.memo"
           class="mt-8"
         />
-
         <OptionalHbarInput
           v-model="state.maxFee"
           class="mt-8"
@@ -58,7 +53,6 @@
         />
       </div>
     </div>
-
     <!-- TODO: Enable Multiple Asset Transfers -->
     <!-- <Button
       color="white"
@@ -68,7 +62,6 @@
       {{ $t("BaseButton.addTransfer1") }}
     </Button> -->
   </div>
-
   <div
     v-if="state.generalErrorText != null"
     class="px-4 py-3 mx-auto mt-10 -mb-8 rounded bg-unburdened-pink w-max"
@@ -77,7 +70,6 @@
       {{ state.generalErrorText }}
     </div>
   </div>
-
   <!-- bottom buttons section -->
   <div class="flex flex-col items-center w-7/12 m-auto mt-10 mb-10">
     <Button
@@ -89,7 +81,6 @@
     >
       {{ state.sendBusyText ?? "Send" }}
     </Button>
-
     <Button
       color="white"
       class="py-2 mt-4 text-sm px-9"
@@ -99,14 +90,13 @@
     </Button>
   </div>
 </template>
-
 <script lang="ts">
 import {
     computed,
-    defineComponent, 
-    onMounted, 
-    reactive, 
-    ref 
+    defineComponent,
+    onMounted,
+    reactive,
+    ref
 } from "vue";
 import { BigNumber } from "bignumber.js";
 import type { AccountId } from "@hashgraph/sdk";
@@ -119,14 +109,12 @@ import OptionalMemo from "../../components/interface/OptionalMemo.vue";
 import Button from "../../components/base/Button.vue";
 import TextInput from "../../components/base/TextInput.vue";
 import { useStore } from "../../store";
-
 export interface Transfer {
     to?: AccountId;
     asset: string; // "HBAR" or token ID (string)
     amount?: BigNumber;
     usd?: string;
 }
-
 export default defineComponent({
   name: "Send",
   components: {
@@ -141,11 +129,9 @@ export default defineComponent({
     const router = useRouter();
     const store = useStore();
     const hashgraph = ref<typeof import("@hashgraph/sdk") | null>(null);
-
     onMounted(async () => {
         hashgraph.value = await import("@hashgraph/sdk");
     });
-    
     let state = reactive({
       accountId: store.accountId,
       showAddModal: false,
@@ -164,17 +150,13 @@ export default defineComponent({
       } as Transfer,
       transfers: [] as Transfer[],
     });
-
     const sendValid = computed(
       () => state.transfer.to != null && state.transfer.amount != null
     );
-
     async function onSend(): Promise<void> {
        if (store.client == null) return;
-      
       state.sendBusyText = "Executing transaction …";
       state.generalErrorText = null;
-
       try {
         await store.client.transfer({
           transfers: [state.transfer],
@@ -184,9 +166,7 @@ export default defineComponent({
             state.sendBusyText = "Waiting for confirmation …";
           },
         });
-
         void store.requestAccountBalance();
-
         // go back to home
         // goal is to see the now PENDING transaction
         // so we can watch it "reach consensus"
@@ -197,15 +177,12 @@ export default defineComponent({
         state.sendBusyText = null;
       }
     }
-
     function removeTransfer(index: number) {
       state.transfers.splice(index, 1);
     }
-
     function onCancel() {
       router.back();
     }
-
     return {
       state,
       sendValid,
