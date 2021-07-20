d5d4ff0ad795e6678cf28d25f49d0ea8d2311d59
Chris Campbell <chris@launchbadge.com>
2021-07-20 09:37:21 -0700

    wip: create account tool

---
+++ src/pages/interface/CreateAccount.vue
@@ -0,0 +1,45 @@
+<template>
+  <Headline />
+  
+  <Hint />
+  
+  <form @submit.prevent="handleSubmit">
+    <AssetInput />
+
+    <InputError />
+    
+    <Button
+      type="submit"
+      @click.stop="handleSubmit"
+    />
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+import Headline from "../../components/interface/Headline.vue";
+import Hint from "../../components/interface/Hint.vue";
+import AssetInput from "../../components/interface/AssetInput.vue";
+import InputError from "../../components/base/InputError.vue";
+import Button from "../../components/base/Button.vue";
+
+export default defineComponent({
+  name: "CreateAccount",
+  components: {
+    Headline,
+    Hint,
+    AssetInput,
+    InputError,
+    Button
+  },
+  setup() {
+    const state = reactive({
+    });
+
+    return {
+      state
+    }
+  }
+})
+</script>
