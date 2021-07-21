ab5e7219fc955124fcbade8731c3cfe9b8f58285
Danielle Smith <danielle.smith@launchbadge.com>
2021-07-21 14:23:33 -0700

    Fixed Unit Converter tool, removed unit.ts NOTE: Issues with table format is fixed in another ticket by altering min-w
    Signed-off-by: Danielle Smith <danielle.smith@launchbadge.com>

--- src/pages/interface/ConvertUnits.vue
+++ src/pages/interface/ConvertUnits.vue
@@ -1,14 +1,9 @@
 <template>
-  <Headline
-    :title="$t('InterfaceConvertUnits')"
-    parent="tools"
-  />
+  <Headline :title="$t('InterfaceConvertUnits')" parent="tools" />
   <div
-    class="items-center pb-10 mt-8 font-medium border-b text-carbon border-cerebral-grey dark:border-midnight-express"
+    class="items-center pb-10 mt-8 font-medium border-b text-carbon dark:text-silver-polish border-cerebral-grey dark:border-midnight-express"
   >
-    <div class="m-16 page-subtitle">
-      {{ $t("ourHelpfulConversionTool") }}
-    </div>
+    <div class="m-16 page-subtitle">{{ $t("ourHelpfulConversionTool") }}</div>
 
     <div class="flex flex-wrap items-stretch m-16">
       <div class="w-5/12 m-auto block-left">
@@ -25,18 +20,15 @@
           <Select
             v-model="state.selectedLeft"
             class="m-2"
-            :options="state.units"
-            :placeholder="state.units[0].name"
+            :options="units"
+            :placeholder="units[0].name"
             @update:modelValue="handleSelect"
           />
         </div>
       </div>
 
       <div class="self-center block-center">
-        <img
-          src="../../assets/swap.svg"
-          alt="Swap"
-        >
+        <img src="../../assets/swap.svg" alt="Swap" />
       </div>
 
       <div class="w-5/12 m-auto block-right">
@@ -50,204 +42,198 @@
           <Select
             v-model="state.selectedRight"
             class="m-2"
-            :options="state.units"
+            :options="units"
             @update:modelValue="handleSelect"
           />
         </div>
       </div>
     </div>
 
-    <div class="items-center m-16 unit-table">
+    <div class="items-center lg:m-16 md:m-16 unit-table overflow-auto">
       <div
-        class="mt-16 mb-6 font-semibold text-center unit-table-header"
-      >
-        {{ $t("referenceGuideTitle") }}
-      </div>
+        class="mt-16 mb-8 font-semibold text-center unit-table-header"
+      >{{ $t("referenceGuideTitle") }}</div>
 
-      <div class="table min-w-full m-auto">
-        <div
-          v-for="unit in state.units"
+      <div class="table w-full">
+        <tr
+          v-for="unit in units"
           :key="unit.name"
-          class="table-row-group"
+          class="flex mb-8 border-b dark:border-b-silver-polish overflow-wrap"
         >
-          <div class="table-row">
-            <span
-              class="table-cell w-1/3 py-3 pl-6 border-b border-cerebral-grey"
-            >{{ unit.name }}</span>
-            <span class="table-cell w-1/3 py-3 pl-10 border-b border-cerebral-grey">
-              <!-- {{ unit.amount }} {{ unit.symbol }} -->
-            </span>
-            <!-- <span
-              class="table-cell w-1/3 py-3 pl-16 border-b border-cerebral-grey"
-                        >{{ unit.amountInHbar }} ℏ</span>-->
-          </div>
-        </div>
+          <td class="w-1/3 flex overflow-wrap">
+            {{ unit.name }}
+          </td>
+          <td class="w-1/3 flex overflow-wrap">
+            {{ unit.amount }}
+          </td>
+          <td class="w-1/3 flex overflow-wrap">
+            {{ unit.amountInHbar }} {{ unit.symbol }}
+          </td>
+        </tr>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, computed } from "vue";
 import { BigNumber } from "bignumber.js";
 
 import Headline from "../../components/interface/Headline.vue";
 import TextInput from "../../components/base/TextInput.vue";
 import Select, { Option } from "../../components/base/Select.vue";
 
+
 export default defineComponent({
-    name: "ConvertUnits",
-    components: {
-        Headline,
-        TextInput,
-        Select,
-    },
-    setup() {
-        BigNumber.config({
-            DECIMAL_PLACES: 64,
-            RANGE: 500,
-            EXPONENTIAL_AT: 64,
-        });
+  name: "ConvertUnits",
+  components: {
+    Headline,
+    TextInput,
+    Select,
+  },
 
-        const state = reactive({
-            selectedLeft: "Tinybar",
-            selectedRight: "Hbar",
-            valueLeft: "100000000",
-            valueRight: "1",
-            units: [
-                {
-                    name: "Tinybar",
-                    iconLight: null,
-                    iconDark: null,
-                    symbol: "tℏ",
-                    amount: "100,000,000",
-                    amountInHbar: "1",
-                },
+  setup() {
+    BigNumber.config({
+      DECIMAL_PLACES: 64,
+      RANGE: 500,
+      EXPONENTIAL_AT: 64,
+    });
 
-                {
-                    name: "Millibar",
-                    iconLight: null,
-                    iconDark: null,
-                    symbol: "μℏ",
-                    amount: "1,000,000",
-                    amountInHbar: "1",
-                },
 
-                {
-                    name: "Hbar",
-                    iconLight: null,
-                    iconDark: null,
-                    symbol: "ℏ",
-                    amount: "1",
-                    amountInHbar: "1",
-                },
-                {
-                    name: "Kilobar",
-                    iconLight: null,
-                    iconDark: null,
-                    symbol: "kℏ",
-                    amount: "1",
-                    amountInHbar: "1,000",
-                },
-                {
-                    name: "Megabar",
-                    iconLight: null,
-                    iconDark: null,
-                    symbol: "Mℏ",
-                    amount: "1",
-                    amountInHbar: "1,000,000",
-                },
-                {
-                    name: "Gigabar",
-                    iconLight: null,
-                    iconDark: null,
-                    symbol: "Gℏ",
-                    amount: "1",
-                    amountInHbar: "1,000,000,000",
-                },
-            ] as unknown as Option[],
-        });
+    const units = [
+      {
+        name: "Tinybar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "tℏ",
+        amount: "100,000,000",
+        amountInHbar: "1",
+      },
+      {
+        name: "Millibar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "μℏ",
+        amount: "1,000,000",
+        amountInHbar: "1",
+      },
+      {
+        name: "Hbar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "ℏ",
+        amount: "1",
+        amountInHbar: "1",
+      },
+      {
+        name: "Kilobar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "kℏ",
+        amount: "1",
+        amountInHbar: "1,000",
+      },
+      {
+        name: "Megabar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "Mℏ",
+        amount: "1",
+        amountInHbar: "1,000,000",
+      },
+      {
+        name: "Gigabar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "Gℏ",
+        amount: "1",
+        amountInHbar: "1,000,000,000",
+      },
+    ] as unknown as Option[];
 
-        function handleInputValueLeft(value: string): void {
-            state.valueLeft = value;
-            computeValueRight();
-            computeValueLeft();
-        }
+    const state = reactive({
+      selectedLeft: "Tinybar",
+      selectedRight: "Hbar",
+      valueLeft: "100000000",
+      valueRight: "1",
+    });
 
-        function handleInputValueRight(value: string): void {
-            state.valueRight = value;
-            computeValueLeft();
-            computeValueRight();
-        }
 
-        async function handleSelect(): Promise<void> {
-            const { Hbar, HbarUnit } = await import("@hashgraph/sdk");
-            let unitRight = state.selectedRight;
-            let unitLeft = state.selectedLeft;
-            let hLeft = new Hbar(
-                new BigNumber(state.valueLeft),
-                await getHbarUnit(unitLeft)
-            );
-            state.valueRight = hLeft.to(await getHbarUnit(unitRight)).toString();
-            console.log(state.valueRight);
+    function handleInputValueLeft(value: string): void {
+      if (isNaN(parseInt(value))) {
+        return;
+      }
+      state.valueLeft = value;
+      computeValueRight();
+    }
 
-            let test = new Hbar(111, HbarUnit.Gigabar);
-            console.log(`Test: ${test.to(HbarUnit.Tinybar).toString()}`);
-        }
+    function handleInputValueRight(value: string): void {
+      if (isNaN(parseInt(value))) {
+        return;
+      }
+      state.valueRight = value;
+      computeValueLeft();
+    }
 
-        async function getHbarUnit(value: string): Promise<import("@hashgraph/sdk").HbarUnit> {
-            const { HbarUnit } = await import("@hashgraph/sdk");
-            switch (value) {
-                case "Tinybar":
-                    return HbarUnit.Tinybar;
-                case "Microbar":
-                    return HbarUnit.Microbar;
-                case "Millibar":
-                    return HbarUnit.Millibar;
-                case "Hbar":
-                    return HbarUnit.Hbar;
-                case "Kilobar":
-                    return HbarUnit.Kilobar;
-                case "Megabar":
-                    return HbarUnit.Megabar;
-                case "Gigabar":
-                    return HbarUnit.Gigabar;
-                default:
-                    return HbarUnit.Hbar;
-            }
-        }
+    async function handleSelect(): Promise<void> {
+      computeValueLeft();
+      computeValueRight();
+    }
 
-        async function computeValueLeft(): Promise<void> {
-            const { Hbar } = await import("@hashgraph/sdk");
-            let unitRight = state.selectedRight;
-            let unitLeft = state.selectedLeft;
-            let hRight = new Hbar(
-                new BigNumber(state.valueRight),
-                await getHbarUnit(unitRight)
-            );
-            state.valueLeft = hRight.to(await getHbarUnit(unitLeft)).toString();
-        }
+    async function getHbarUnit(value: string): Promise<import("@hashgraph/sdk").HbarUnit> {
+      const { HbarUnit } = await import("@hashgraph/sdk");
+      switch (value) {
+        case "Tinybar":
+          return HbarUnit.Tinybar;
+        case "Microbar":
+          return HbarUnit.Microbar;
+        case "Millibar":
+          return HbarUnit.Millibar;
+        case "Hbar":
+          return HbarUnit.Hbar;
+        case "Kilobar":
+          return HbarUnit.Kilobar;
+        case "Megabar":
+          return HbarUnit.Megabar;
+        case "Gigabar":
+          return HbarUnit.Gigabar;
+        default:
+          return HbarUnit.Hbar;
+      }
+    }
 
-        async function computeValueRight(): Promise<void> {
-            const { Hbar } = await import("@hashgraph/sdk");
-            let unitRight = state.selectedRight;
-            let unitLeft = state.selectedLeft;
-            let hLeft = new Hbar(
-                new BigNumber(state.valueLeft),
-                await getHbarUnit(unitLeft)
-            );
-            state.valueRight = hLeft.to(await getHbarUnit(unitRight)).toString();
-        }
+    async function computeValueLeft(): Promise<void> {
+      const { Hbar } = await import("@hashgraph/sdk");
+      let unitRight = state.selectedRight;
+      let unitLeft = state.selectedLeft;
+      let hRight = new Hbar(
+        new BigNumber(state.valueRight),
+        await getHbarUnit(unitRight)
+      );
+      state.valueLeft = hRight.to(await getHbarUnit(unitLeft)).toString();
+    }
 
-        return {
-            state,
-            handleInputValueLeft,
-            handleInputValueRight,
-            handleSelect,
-            computeValueLeft,
-            computeValueRight,
-            getHbarUnit,
-        };
-    },
+    async function computeValueRight(): Promise<void> {
+      const { Hbar } = await import("@hashgraph/sdk");
+      let unitRight = state.selectedRight;
+      let unitLeft = state.selectedLeft;
+      let hLeft = new Hbar(
+        new BigNumber(state.valueLeft),
+        await getHbarUnit(unitLeft)
+      );
+      state.valueRight = hLeft.to(await getHbarUnit(unitRight)).toString();
+    }
+
+    return {
+      state,
+      units,
+      handleInputValueLeft,
+      handleInputValueRight,
+      handleSelect,
+      computeValueLeft,
+      computeValueRight,
+      getHbarUnit,
+    };
+  },
 });
 </script>
