5692a1a446792780073c419a7420bd0665130c11
Danielle Smith <danielle.smith@launchbadge.com>
2021-07-22 14:37:31 -0700

    Fixed Hint; will remain same size regardless of screen, text and icon will remain centered; removed QR code remnants.
    Signed-off-by: Danielle Smith <danielle.smith@launchbadge.com>
    
    Removed qrcode.vue from package.json
    
    Signed-off-by: Danielle Smith <danielle.smith@launchbadge.com>
    
    Removed QRcodeVue from Account
    
    Signed-off-by: Danielle Smith <danielle.smith@launchbadge.com>

--- src/pages/access/Account.vue
+++ src/pages/access/Account.vue
@@ -138,7 +138,7 @@ export default defineComponent({
     AccountIdInput,
     InputError,
     Button,
-    CustomerSupportButton
+    CustomerSupportButton,
   },
   setup() {
     const container = useContainer();
