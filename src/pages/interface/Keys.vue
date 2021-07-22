91426ed111af45386da3eebb32b8ae57a443b5b2
Chris Campbell <chris@launchbadge.com>
2021-07-22 16:43:42 -0700

    feat: create by private key
    Signed-off-by: Chris Campbell <chris@launchbadge.com>

--- src/pages/interface/Keys.vue
+++ src/pages/interface/Keys.vue
@@ -1,7 +1,10 @@
 <template>
   <Headline :title="$t('InterfaceKeys.header')" />
 
-  <div v-if="privateKey" class="md:px-8 py-4 my-8 max-w-3xl w-full mx-auto">
+  <div
+    v-if="privateKey"
+    class="md:px-8 py-4 my-8 max-w-3xl w-full mx-auto"
+  >
     <KeyInput
       :model-value="privateKey"
       can-copy
@@ -11,7 +14,10 @@
     />
   </div>
 
-  <div v-if="publicKey" class="md:px-8 py-4 my-8 max-w-3xl w-full mx-auto">
+  <div
+    v-if="publicKey"
+    class="md:px-8 py-4 my-8 max-w-3xl w-full mx-auto"
+  >
     <KeyInput
       :model-value="publicKey"
       can-copy
