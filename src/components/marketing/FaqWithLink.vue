d089d53476ccd14e2d1d592ca4c75aa356d3ed52
Mason Meier <masonmeier@pop-os.localdomain>
2021-07-26 16:42:54 -0700

    added propper linking to marketing page translations
    Signed-off-by: Mason Meier <masonmeier@pop-os.localdomain>

---
+++ src/components/marketing/FaqWithLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div
+      class="mb-2 text-base font-semibold leading-tight text-abbadon-black dark:text-white"
+    >
+      {{ question }}
+    </div>
+    <div
+      class="mb-8 text-sm font-normal leading-relaxed text-carbon dark:text-white"
+    >
+      {{ firstAnswer }} <a
+        class="underline"
+        :href="link"
+      >{{ secondAnswer }}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "FaqWithLink",
+  props: {
+    question: { type: String, required: true },
+    firstAnswer: { type: String, required: true },
+    secondAnswer: { type: String, required: true },
+    link: { type: String, required: true },
+  },
+};
+</script>
+
+
