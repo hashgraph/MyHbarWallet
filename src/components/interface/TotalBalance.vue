8e2a119ec14ee83235e18e2b73030320c719f5f7
Danielle Smith <danielle.smith@launchbadge.com>
2021-07-27 13:57:31 -0700

    Created refresh balance button, in use in TotalBalance component, ready for review.
    Signed-off-by: Danielle Smith <danielle.smith@launchbadge.com>

--- src/components/interface/TotalBalance.vue
+++ src/components/interface/TotalBalance.vue
@@ -2,34 +2,44 @@
   <div class="mt-5 mb-5 text-base text-black-out dark:text-white">
     {{ $t("InterfaceTotalBalance.totalvalue") }}
   </div>
-  <div class="flex items-center">
-    <Image
-      class="h-12 mr-7 md:mr-5 md:h-16"
-      :light="wallet"
-      :dark="wallet"
-      alt="wallet logo"
-    />
+  <div class="flex-col items-center text-center mb-4">
+    <div class="flex mb-4">
+      <Image
+        class="h-12 mr-7 md:mr-5 md:h-16"
+        :light="wallet"
+        :dark="wallet"
+        alt="wallet logo"
+      />
 
-    <div
-      v-if="balanceInUsd"
-      class="flex justify-center text-5xl font-semibold leading-10  md:text-6xl text-mountain-meadow"
-    >
-      <span class="self-start text-2xl">$</span>{{ balanceInUsd }}
+      <div
+        v-if="balanceInUsd"
+        class="flex justify-center text-5xl font-semibold leading-10  md:text-6xl text-mountain-meadow"
+      >
+        <span class="self-start text-2xl">$</span>{{ state.balanceInUsd ?? balanceInUsd }}
+      </div>
     </div>
+
+    <RefreshButton
+      class="m-auto"
+      @refresh="refreshBalance"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, reactive } from "vue";
 
 import { useStore } from "../../store";
 import Image from "../base/Image.vue";
 import wallet from "../../assets/img_wallet.svg";
 
+import RefreshButton from "./RefreshBalance.vue";
+
 export default defineComponent({
   name: "TotalBalance",
   components: {
     Image,
+    RefreshButton
   },
   setup() {
     const store = useStore();
@@ -47,9 +57,20 @@ export default defineComponent({
       });
     });
 
+
+    const state = reactive({
+      balanceInUsd: balanceInUsd.value as string | null
+    });
+
+    function refreshBalance(e: { balance: string }): void {
+      state.balanceInUsd = e.balance;
+    }
+
     return {
       wallet,
       balanceInUsd,
+      refreshBalance,
+      state
     };
   },
 });
