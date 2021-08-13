7d7e4851dc37742e7c406bfac2da84ff6f509ce4
masonmeier <57198981+masonmeier@users.noreply.github.com>
2021-08-12 17:39:29 -0700

    WIP: test requires refactor associate.spec.ts
    Signed-off-by: masonmeier <57198981+masonmeier@users.noreply.github.com>

--- tests/cypress/integration/wallet/Associate.spec.ts
+++ tests/cypress/integration/wallet/Associate.spec.ts
@@ -19,7 +19,29 @@ describe("Tool: Associate Token", () => {
             .click();
     });
 
-    // it("can associate a token", async () => {
+    //TODO: refactor cypress commands to take credentials for client construction instead of 6189
+    //TODO: refactor test to create token as one account and associate as another
+    it("can associate a token", async () => {
+        let tokenId: string = "";
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        cy.createToken().then((_tokenId: string) => {
+            tokenId = _tokenId;
+        }).then(() => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            cy.dissociateToken(tokenId)
+            }
+        ).then(() => {
+            cy.get("[data-cy-token-id] input")
+                .filter(":visible")
+                .type(tokenId)
+                .get("[data-cy-submit]")
+                .click()
+        })
+    });
+
+    // it("can identify an already associated token", async () => {
     //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //     // @ts-ignore
     //     cy.createToken().then((tokenId: string) => {
@@ -29,29 +51,14 @@ describe("Tool: Associate Token", () => {
     //                 .filter(":visible")
     //                 .type(tokenIdString)
     //                 .get("[data-cy-submit]")
-    //                 .click();
+    //                 .click()
+    //                 .get("[data-cy-input-error]")
+    //                 .should("be.visible")
+    //                 .should("contain", "This account and token are already associated.")
     //         }
     //     });
+    //
     // });
-
-    it("can identify an already associated token", async () => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        cy.createToken().then((tokenId: string) => {
-            const tokenIdString = tokenId.toString();
-            if (tokenId) {
-                cy.get("[data-cy-token-id] input")
-                    .filter(":visible")
-                    .type(tokenIdString)
-                    .get("[data-cy-submit]")
-                    .click()
-                    .get("[data-cy-input-error]")
-                    .should("be.visible")
-                    .should("contain", "This account and token are already associated.")
-            }
-        });
-
-    });
     //
     // it("can identify a token that requires KYC", () => {});
 });
--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -4,7 +4,9 @@ import {
     Client,
     AccountId,
     PrivateKey,
-    TokenId,
+    AccountCreateTransaction,
+    Hbar,
+    TokenDissociateTransaction,
 } from "@hashgraph/sdk";
 
 const { KEY_PRIVATE_KEY, KEY_ACCOUNT_ID } = Cypress.env();
@@ -30,6 +32,64 @@ Cypress.Commands.add("login", (privateKey: string, accountId: string) => {
     cy.window().its("$store").invoke("login", privateKey, accountId);
     cy.window().its("$router").invoke("push", { name: "home" });
 });
+
+Cypress.Commands.add("createAccount", async () => {
+    let client;
+
+    try {
+        client = Client.forTestnet().setOperator(
+            AccountId.fromString(KEY_ACCOUNT_ID),
+            PrivateKey.fromString(KEY_PRIVATE_KEY),
+        );
+    } catch {
+        throw new Error(
+            "Environment variables HEDERA_NETWORK, OPERATOR_ID, and OPERATOR_KEY are required."
+        );
+    }
+
+    const newKey = PrivateKey.generate();
+
+    console.log(`private key = ${newKey}`);
+    console.log(`public key = ${newKey.publicKey}`);
+
+    const response = await new AccountCreateTransaction()
+        .setInitialBalance(new Hbar(10)) // 10 h
+        .setKey(newKey.publicKey)
+        .execute(client);
+
+    const receipt = await response.getReceipt(client);
+
+    console.log(`account id = ${receipt.accountId}`);
+
+    if(receipt.accountId){
+        return (receipt.accountId.toString());
+    }
+})
+
+Cypress.Commands.add("dissociateToken", async (tokenId) => {
+    let client: Client | undefined;
+
+    try {
+        client = Client.forTestnet().setOperator(
+            AccountId.fromString(KEY_ACCOUNT_ID),
+            PrivateKey.fromString(KEY_PRIVATE_KEY),
+        );
+    } catch {
+        cy.log("environment variables not set");
+    }
+
+    const key = client?.operatorPublicKey;
+    const account = client?.operatorAccountId;
+
+    if (client && key && account) {
+        let res = await new TokenDissociateTransaction()
+            .setAccountId(account)
+            .setTokenIds([tokenId])
+            .execute(client);
+
+        await res.getReceipt(client);
+    }
+})
 
 Cypress.Commands.add(
     "createToken",
