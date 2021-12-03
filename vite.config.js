84391e469da68a1f90d74a783e0552178ff9243f
Jack Ta <jack.th.ta@gmail.com>
2021-12-03 11:04:03 -0800

    feat: add Buy option for purchasing hbar w/ MoonPay widget
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default async function ({ mode }) {
                 "v2.api.testnet.kabuto.sh",
             ].join(" "),
         "font-src 'self' data:",
+        "frame-src 'self' https://buy.moonpay.com",
         isProduction ? "style-src 'self'" : "style-src 'self' 'unsafe-inline'",
     ];
 
@@ -73,6 +74,7 @@ export default async function ({ mode }) {
             __APP_LAST_COMMIT_SHORT_HASH__: JSON.stringify(
                 lastCommit.shortHash
             ),
+            __MOONPAY_API_KEY__: JSON.stringify(process.env.MOONPAY_API_KEY) // MOONPAY_API_KEY from netlify environment vars
         },
         build: {
             outDir: isElectron ? electronOutDir : webOutDir,
