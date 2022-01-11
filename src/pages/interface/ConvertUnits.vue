e21b0e4beed8db8cc72728b984b9b6dbc7fa5c06
CL <78613993+derdis14@users.noreply.github.com>
2022-01-11 22:41:32 +0100

    fix: hbar unit reference guide
    Signed-off-by: CL <78613993+derdis14@users.noreply.github.com>

--- src/pages/interface/ConvertUnits.vue
+++ src/pages/interface/ConvertUnits.vue
@@ -94,7 +94,7 @@
           </td>
           
           <td class="w-1/3 flex overflow-wrap">
-            {{ unit.amountInHbar }} {{ units[2].symbol }}
+            {{ unit.amountInHbar }} ℏ
           </td>
         </tr>
       </div>
@@ -137,11 +137,19 @@ export default defineComponent({
         amountInHbar: "1",
       },
       {
-        name: "Millibar",
+        name: "Microbar",
         iconLight: null,
         iconDark: null,
         symbol: "μℏ",
         amount: "1,000,000",
+        amountInHbar: "1",
+      },
+      {
+        name: "Millibar",
+        iconLight: null,
+        iconDark: null,
+        symbol: "mℏ",
+        amount: "1,000",
         amountInHbar: "1",
       },
       {
