679bfbc2ac36c36d2a6389861b3552b96d40b5b3
Chris Campbell <chris@launchbadge.com>
2022-10-27 13:06:56 -0700

    fix: update staking fetch to wait 5 seconds, mirror now supports this
    Signed-off-by: Chris Campbell <chris@launchbadge.com>

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,12 @@ import { BigNumber } from "bignumber.js";
 import { defineStore } from "pinia";
 
 import { Wallet } from "./domain/wallet/abstract";
-import { AccountBalance, MirrorAccountInfo, NetworkNodeStakingInfo, SimpleHederaClient } from "./services/hedera";
+import {
+    AccountBalance,
+    MirrorAccountInfo,
+    NetworkNodeStakingInfo,
+    SimpleHederaClient,
+} from "./services/hedera";
 import { useContainer } from "./hooks/container";
 import i18n from "./i18n";
 import { PrivateKeySoftwareWallet } from "./domain/wallet/software-private-key";
@@ -93,13 +98,15 @@ export const useStore = defineStore({
 
         getAccountStakingInfo(): MirrorAccountInfo | null {
             return this.accountStakingInfo;
-        }
+        },
     },
 
     actions: {
         async login(privateKey: string, accountIdStr: string): Promise<void> {
             if (__TEST__) {
-                const { PrivateKey, AccountId } = await import("@hashgraph/sdk");
+                const { PrivateKey, AccountId } = await import(
+                    "@hashgraph/sdk"
+                );
                 const key = PrivateKey.fromString(privateKey);
                 const accountId = AccountId.fromString(accountIdStr);
                 const wallet = new PrivateKeySoftwareWallet(key);
@@ -118,11 +125,15 @@ export const useStore = defineStore({
         },
 
         async networkPing(): Promise<boolean> {
-            const { AccountBalanceQuery, AccountId } = await import("@hashgraph/sdk");
+            const { AccountBalanceQuery, AccountId } = await import(
+                "@hashgraph/sdk"
+            );
             const accountId = AccountId.fromString("0.0.2");
 
             try {
-                new AccountBalanceQuery().setNodeAccountIds([accountId]).setAccountId(accountId);
+                new AccountBalanceQuery()
+                    .setNodeAccountIds([accountId])
+                    .setAccountId(accountId);
                 this.networkStatus = true;
             } catch (error) {
                 this.networkStatus = false;
@@ -181,112 +192,196 @@ export const useStore = defineStore({
 
         async fetchNetworkNodeStakingInfo() {
             const container = useContainer();
-            this.nodeStakingInfo = await container.cradle.hedera.getNodeStakingInfo(this.network);
+            this.nodeStakingInfo =
+                await container.cradle.hedera.getNodeStakingInfo(this.network);
         },
 
         async fetchAccountStakingInfo() {
             if (this.accountId == null || this.getClient == null) {
-                throw new Error('tried to get account staking info without being logged in!');
+                throw new Error(
+                    "tried to get account staking info without being logged in!"
+                );
             }
             const container = useContainer();
 
             // HACK: local storage is being used because the Hedera mirror node is up to 15 minutes delayed in accurary
-            const lastUpdate = localStorage.getItem(`${this.network}.${this.accountId.toString()}.last-updated`);
-            const node = localStorage.getItem(`${this.network}.${this.accountId.toString()}.staking-to`);
-            const fifteenMinutes = 15 * 60 * 1000;
-            if (lastUpdate == null || node == null || (Date.now() - +(new Date(parseInt(lastUpdate)))) > fifteenMinutes) {
-                this.accountStakingInfo = await container.cradle.hedera.getMirrorAccountInfo(this.network, this.accountId);
-                localStorage.setItem(`${this.network}.${this.accountId.toString()}.staking-to`, this.accountStakingInfo.staked_node_id?.toString() ?? '');
-                localStorage.setItem(`${this.network}.${this.accountId.toString()}.last-updated`, Date.now().toString());
+            const lastUpdate = localStorage.getItem(
+                `${this.network}.${this.accountId.toString()}.last-updated`
+            );
+            const node = localStorage.getItem(
+                `${this.network}.${this.accountId.toString()}.staking-to`
+            );
+
+            if (
+                lastUpdate == null ||
+                node == null ||
+                Date.now() - +new Date(parseInt(lastUpdate)) > 5000
+            ) {
+                this.accountStakingInfo =
+                    await container.cradle.hedera.getMirrorAccountInfo(
+                        this.network,
+                        this.accountId
+                    );
+                localStorage.setItem(
+                    `${this.network}.${this.accountId.toString()}.staking-to`,
+                    this.accountStakingInfo.staked_node_id?.toString() ?? ""
+                );
+                localStorage.setItem(
+                    `${this.network}.${this.accountId.toString()}.last-updated`,
+                    Date.now().toString()
+                );
             } else {
-                this.accountStakingInfo = { staked_node_id: node === '' ? undefined : parseInt(node), account: this.accountId.toString() };
+                this.accountStakingInfo = {
+                    staked_node_id: node === "" ? undefined : parseInt(node),
+                    account: this.accountId.toString(),
+                };
             }
         },
 
         async updateStake(declineRewards: boolean, node: number | null) {
             if (this.accountId == null || this.getClient == null) {
-                throw new Error('tried to update stake without being logged in!');
+                throw new Error(
+                    "tried to update stake without being logged in!"
+                );
             }
 
-            await this.getClient?.updateStaking({ node, declineRewards, accountId: this.accountId });
+            await this.getClient?.updateStaking({
+                node,
+                declineRewards,
+                accountId: this.accountId,
+            });
 
             //HACK: local storage is being used because the Hedera mirror node is up to 15 minutes delayed in accuracy
-            localStorage.setItem(`${this.network}.${this.accountId.toString()}.staking-to`, node ? node.toString() : '');
-            localStorage.setItem(`${this.network}.${this.accountId.toString()}.last-updated`, Date.now().toString());
+            localStorage.setItem(
+                `${this.network}.${this.accountId.toString()}.staking-to`,
+                node ? node.toString() : ""
+            );
+            localStorage.setItem(
+                `${this.network}.${this.accountId.toString()}.last-updated`,
+                Date.now().toString()
+            );
         },
 
         async errorMessage(error: Error): Promise<string> {
             const { Status, StatusError } = await import("@hashgraph/sdk");
-            const { StatusCodes, TransportStatusError } = await import("@ledgerhq/hw-transport");
+            const { StatusCodes, TransportStatusError } = await import(
+                "@ledgerhq/hw-transport"
+            );
 
             if (error instanceof StatusError) {
                 switch (error.status) {
                     case Status.AccountDeleted:
                     case Status.AccountExpiredAndPendingRemoval:
                     case Status.AccountIdDoesNotExist:
-                        return i18n.global.t("Common.Error.AccountDoesNotExist").toString();
+                        return i18n.global
+                            .t("Common.Error.AccountDoesNotExist")
+                            .toString();
                     case Status.AccountKycNotGrantedForToken:
                     case Status.AccountFrozenForToken:
-                        return i18n.global.t("Common.Error.PermissionDeniedAccountToken").toString();
+                        return i18n.global
+                            .t("Common.Error.PermissionDeniedAccountToken")
+                            .toString();
                     case Status.AccountRepeatedInAccountAmounts:
-                        return i18n.global.t("Common.Error.CantSendToYourself").toString();
+                        return i18n.global
+                            .t("Common.Error.CantSendToYourself")
+                            .toString();
                     case Status.Busy:
                     case Status.Unknown:
                     case Status.Unauthorized:
-                        return i18n.global.t("Common.Error.NetworkUnavailable").toString();
+                        return i18n.global
+                            .t("Common.Error.NetworkUnavailable")
+                            .toString();
                     case Status.DuplicateTransaction:
-                        return i18n.global.t("Common.Error.DuplicateTx").toString();
+                        return i18n.global
+                            .t("Common.Error.DuplicateTx")
+                            .toString();
                     case Status.EmptyTokenTransferAccountAmounts:
                     case Status.EmptyTokenTransferBody:
                     case Status.EmptyTransactionBody:
-                        return i18n.global.t("Common.Error.EmptyTransaction").toString();
+                        return i18n.global
+                            .t("Common.Error.EmptyTransaction")
+                            .toString();
                     case Status.FileContentEmpty:
-                        return i18n.global.t("Common.Error.FileEmpty").toString();
+                        return i18n.global
+                            .t("Common.Error.FileEmpty")
+                            .toString();
                     case Status.FileDeleted:
-                        return i18n.global.t("Common.Error.FileDeleted").toString();
+                        return i18n.global
+                            .t("Common.Error.FileDeleted")
+                            .toString();
                     case Status.MaxFileSizeExceeded:
                         return i18n.global.t("Common.Error.BigFile").toString();
                     case Status.MemoTooLong:
-                        return i18n.global.t("Common.Error.MemoTooLong").toString();
+                        return i18n.global
+                            .t("Common.Error.MemoTooLong")
+                            .toString();
                     case Status.MessageSizeTooLarge:
-                        return i18n.global.t("Common.Error.BigMessage").toString();
+                        return i18n.global
+                            .t("Common.Error.BigMessage")
+                            .toString();
                     case Status.Ok:
                     case Status.Success:
                         return i18n.global.t("Common.Error.Ok").toString();
                     case Status.PayerAccountNotFound:
-                        return i18n.global.t("Common.Error.PayerNotReal").toString();
+                        return i18n.global
+                            .t("Common.Error.PayerNotReal")
+                            .toString();
                     case Status.PayerAccountUnauthorized:
-                        return i18n.global.t("Common.Error.PayerFrozen").toString();
+                        return i18n.global
+                            .t("Common.Error.PayerFrozen")
+                            .toString();
                     case Status.ReceiptNotFound:
-                        return i18n.global.t("Common.Error.NoReceipt").toString();
+                        return i18n.global
+                            .t("Common.Error.NoReceipt")
+                            .toString();
                     case Status.RecordNotFound:
-                        return i18n.global.t("Common.Error.NoRecord").toString();
+                        return i18n.global
+                            .t("Common.Error.NoRecord")
+                            .toString();
                     case Status.ResultSizeLimitExceeded:
-                        return i18n.global.t("Common.Error.BigResults").toString();
+                        return i18n.global
+                            .t("Common.Error.BigResults")
+                            .toString();
                     case Status.TokenAlreadyAssociatedToAccount:
-                        return i18n.global.t("Common.Error.TokenAlreadyAssociated").toString();
+                        return i18n.global
+                            .t("Common.Error.TokenAlreadyAssociated")
+                            .toString();
                     case Status.TokenNotAssociatedToAccount:
-                        return i18n.global.t("Common.Error.TokenNotAssociated").toString();
+                        return i18n.global
+                            .t("Common.Error.TokenNotAssociated")
+                            .toString();
                     case Status.TokenNotAssociatedToFeeCollector:
-                        return i18n.global.t("Common.Error.TokenNoFeeCollector").toString();
+                        return i18n.global
+                            .t("Common.Error.TokenNoFeeCollector")
+                            .toString();
                     case Status.TokenHasNoWipeKey:
                     case Status.TokenHasNoFeeScheduleKey:
                     case Status.TokenHasNoFreezeKey:
                     case Status.TokenHasNoKycKey:
                     case Status.TokenHasNoSupplyKey:
-                        return i18n.global.t("Common.Error.TokenConfigBroken").toString();
+                        return i18n.global
+                            .t("Common.Error.TokenConfigBroken")
+                            .toString();
                     case Status.TokenWasDeleted:
-                        return i18n.global.t("Common.Error.TokenDeleted").toString();
+                        return i18n.global
+                            .t("Common.Error.TokenDeleted")
+                            .toString();
                     case Status.TokensPerAccountLimitExceeded:
-                        return i18n.global.t("Common.Error.TooManyTokens").toString();
+                        return i18n.global
+                            .t("Common.Error.TooManyTokens")
+                            .toString();
                 }
             } else if (error instanceof TransportStatusError) {
                 // need to type assert because @ledgerhq/hw-transport's TransportStatusError doesn't expose statusCode property(?)
                 switch ((error as TransportStatusError).statusCode) {
                     case StatusCodes.CONDITIONS_OF_USE_NOT_SATISFIED:
-                        return i18n.global.t("Common.Error.LedgerDenyRequest").toString();
+                        return i18n.global
+                            .t("Common.Error.LedgerDenyRequest")
+                            .toString();
                     default:
-                        return i18n.global.t("Common.Error.LedgerError").toString();
+                        return i18n.global
+                            .t("Common.Error.LedgerError")
+                            .toString();
                 }
             }
 
